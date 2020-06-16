7c2d4346d1c4faa33dec762f6d528f55aa28178f
Philip John <phil@philipjohn.me.uk>
2020-06-16 18:49:04 +0100

    fix: various bug fixes from initial testing

--- src/Migrator/General/CPTMigrator.php
+++ src/Migrator/General/CPTMigrator.php
@@ -75,29 +75,31 @@ class CPTMigrator implements InterfaceMigrator {
 	 */
 	public function cmd_convert_cpt( $args, $assoc_args ) {
 
+		// Get the to and from post types.
+		$cpt_from = $args[0];
+		$cpt_to   = $args[1];
+
 		// Make sure we have a valid post type.
-		if ( ! \in_array( $assoc_args['from'], get_post_types() ) ) {
+		if ( ! \in_array( $cpt_from, get_post_types() ) ) {
 			// Register the post type temporarily.
-			register_post_type( $assoc_args['from'] );
+			register_post_type( $cpt_from );
 		}
 
 		// Check the "to" post type is valid. It should probably be a core one, really.
-		if ( ! \in_array( $assoc_args['to'], get_post_types() ) ) {
-			WP_CLI::error( sprintf( __('Post type %s does not exist!', $assoc_args['to'] ) ) );
+		if ( ! \in_array( $cpt_to, get_post_types() ) ) {
+			WP_CLI::error( sprintf( __('Post type %s does not exist!' ), $cpt_to ) );
 		}
 
-		$post_id = isset( $assoc_args[ 'post-id' ] ) ? true : false;
+		$post_id = isset( $assoc_args[ 'post-id' ] ) ? (int) $assoc_args['post-id'] : false;
 
 		// Grab the posts to convert then.
 		if ( $post_id ) {
-			$posts = get_posts( [
-				'p'         => $post_id,
-				'post_type' => $assoc_args['from'],
-			] );
+			$posts = [ get_post( $post_id ) ];
 		} else {
 			$posts = get_posts( [
 				'posts_per_page' => -1,
-				'post_type'      => $assoc_args['from'],
+				'post_type'      => $cpt_from,
+				'post_status'    => 'any',
 			] );
 		}
 
@@ -106,16 +108,17 @@ class CPTMigrator implements InterfaceMigrator {
 			WP_CLI::error( 'There are no posts to convert!' );
 		}
 
-		$progress = \WP_CLI\Utils\make_progress_bar( 'Converting posts', $posts );
+		$progress = \WP_CLI\Utils\make_progress_bar( 'Converting posts', count( $posts ) );
 		$failed = 0;
 
 		// Convert the posts!
 		foreach ( $posts as $post ) {
-			$converted = set_post_type( $post->ID, $assoc_args['to'] );
+
+			$converted = set_post_type( $post->ID, $cpt_to );
 
 			if ( 1 === $converted ) {
 				// Set a meta to help us remember what CPT this was converted from.
-				add_post_meta( $post->ID, '_cpt_converted_from', $assoc_args['from'] );
+				add_post_meta( $post->ID, '_cpt_converted_from', $cpt_from );
 			} else {
 				// Log the failure so we know to re-run.
 				$failed++;
