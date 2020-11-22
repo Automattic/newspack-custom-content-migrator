29480bcff40d0f74ba2731f093cc1fe946f72365
Ivan Uravic <iuravic@gmail.com>
2020-11-22 17:47:18 +0100

    Refactor two functions for querying posts with taxonomy to Posts logic class

--- src/MigrationLogic/Posts.php
+++ src/MigrationLogic/Posts.php
@@ -31,4 +31,72 @@ class Posts {
 
 		return $ids;
 	}
+
+	/**
+	 * Gets posts which have tags with taxonomy.
+	 *
+	 * @param string $tag_taxonomy Tag taxonomy.
+	 *
+	 * @return array Array of post IDs found.
+	 */
+	public function get_posts_with_tag_with_taxonomy( $tag_taxonomy ) {
+		global $wpdb;
+		$post_ids = [];
+
+		// TODO: switch to WP_Query instead of raw SQL ( e.g. if ( ! taxonomy_exists( $tag ) ) register_taxonomy( $tag, $object_type ) ).
+		$sql_get_post_ids_with_taxonomy = <<<SQL
+			SELECT DISTINCT wp.ID
+			FROM {$wpdb->prefix}posts wp
+			JOIN {$wpdb->prefix}term_relationships wtr ON wtr.object_id = wp.ID
+			JOIN {$wpdb->prefix}term_taxonomy wtt ON wtt.term_taxonomy_id = wtr.term_taxonomy_id AND wtt.taxonomy = %s
+			JOIN {$wpdb->prefix}terms wt ON wt.term_id = wtt.term_id
+			WHERE wp.post_type = 'post'
+			AND wp.post_status = 'publish'
+			ORDER BY wp.ID;
+SQL;
+		// phpcs:ignore -- false positive, all params are fully sanitized.
+		$results_post_ids               = $wpdb->get_results( $wpdb->prepare( $sql_get_post_ids_with_taxonomy, $tag_taxonomy ), ARRAY_A );
+
+		if ( ! empty( $results_post_ids ) ) {
+			foreach ( $results_post_ids as $result_post_id ) {
+				$post_ids[] = $result_post_id['ID'];
+			}
+		}
+
+		return $post_ids;
+	}
+
+	/**
+	 * For a post ID, gets tags which have the given taxonomy.
+	 *
+	 * @param int    $post_id         Post ID.
+	 * @param string $tag_taxonomy Tag tagxonomy.
+	 *
+	 * @return array Tag names with given taxonomy which this post has.
+	 */
+	public function get_post_tags_with_taxonomy( $post_id, $tag_taxonomy ) {
+		global $wpdb;
+		$names = [];
+
+		// TODO: switch to WP_Query instead of raw SQL ( e.g. if ( ! taxonomy_exists( $tag ) ) register_taxonomy( $tag, $object_type ) ).
+		$sql_get_post_ids_with_taxonomy = <<<SQL
+			SELECT DISTINCT wt.name
+			FROM {$wpdb->prefix}terms wt
+			JOIN {$wpdb->prefix}term_taxonomy wtt ON wtt.taxonomy = %s AND wtt.term_id = wt.term_id
+			JOIN {$wpdb->prefix}term_relationships wtr ON wtt.term_taxonomy_id = wtr.term_taxonomy_id
+			JOIN {$wpdb->prefix}posts wp ON wp.ID = wtr.object_id AND wp.ID = %d
+			WHERE wp.post_type = 'post'
+			AND wp.post_status = 'publish'
+SQL;
+		// phpcs:ignore -- false positive, all params are fully sanitized.
+		$results_names                  = $wpdb->get_results( $wpdb->prepare( $sql_get_post_ids_with_taxonomy, $tag_taxonomy, $post_id ), ARRAY_A );
+		if ( ! empty( $results_names ) ) {
+			foreach ( $results_names as $results_name ) {
+				$names[] = $results_name['name'];
+
+			}
+		}
+
+		return $names;
+	}
 }
--- src/Migrator/General/CoAuthorPlusMigrator.php
+++ src/Migrator/General/CoAuthorPlusMigrator.php
@@ -4,6 +4,7 @@ namespace NewspackCustomContentMigrator\Migrator\General;
 
 use \NewspackCustomContentMigrator\Migrator\InterfaceMigrator;
 use \NewspackCustomContentMigrator\MigrationLogic\CoAuthorPlus;
+use \NewspackCustomContentMigrator\MigrationLogic\Posts;
 use \WP_CLI;
 use \WP_Query;
 
@@ -27,10 +28,16 @@ class CoAuthorPlusMigrator implements InterfaceMigrator {
 	private $coauthorsplus_logic;
 
 	/**
+	 * @var Posts $posts_logic
+	 */
+	private $posts_logic;
+
+	/**
 	 * Constructor.
 	 */
 	private function __construct() {
 		$this->coauthorsplus_logic = new CoAuthorPlus();
+		$this->posts_logic = new Posts();
 	}
 
 	/**
@@ -228,7 +235,7 @@ class CoAuthorPlusMigrator implements InterfaceMigrator {
 		}
 		$tag_taxonomy = $args[0];
 
-		$post_ids = $this->get_posts_with_tag_with_taxonomy( $tag_taxonomy );
+		$post_ids = $this->posts_logic->get_posts_with_tag_with_taxonomy( $tag_taxonomy );
 		$total_posts  = count( $post_ids );
 
 		WP_CLI::line( sprintf( 'Converting author tags beginning with %s and with taxonomy %s to Guest Authors for %d posts.', $this->tag_author_prefix, $tag_taxonomy, $total_posts ) );
@@ -239,7 +246,7 @@ class CoAuthorPlusMigrator implements InterfaceMigrator {
 
 			$guest_author_ids = [];
 			$errors           = [];
-			$author_names     = $this->get_post_tags_with_taxonomy( $post_id, $tag_taxonomy );
+			$author_names     = $this->posts_logic->get_post_tags_with_taxonomy( $post_id, $tag_taxonomy );
 			foreach ( $author_names as $author_name ) {
 				try {
 					$guest_author_ids[] = $this->coauthorsplus_logic->create_guest_author( [ 'display_name' => $author_name ] );
@@ -411,74 +418,6 @@ class CoAuthorPlusMigrator implements InterfaceMigrator {
 		}
 
 		return $cpt->$get_value_from;
-	}
-
-	/**
-	 * Gets posts which have tags with taxonomy.
-	 *
-	 * @param string $tag_taxonomy Tag taxonomy.
-	 *
-	 * @return array Array of post IDs found.
-	 */
-	public function get_posts_with_tag_with_taxonomy( $tag_taxonomy ) {
-		global $wpdb;
-		$post_ids = [];
-
-		// TODO: switch to WP_Query instead of raw SQL ( e.g. if ( ! taxonomy_exists( $tag ) ) register_taxonomy( $tag, $object_type ) ).
-		$sql_get_post_ids_with_taxonomy = <<<SQL
-			SELECT DISTINCT wp.ID
-			FROM {$wpdb->prefix}posts wp
-			JOIN {$wpdb->prefix}term_relationships wtr ON wtr.object_id = wp.ID
-			JOIN {$wpdb->prefix}term_taxonomy wtt ON wtt.term_taxonomy_id = wtr.term_taxonomy_id AND wtt.taxonomy = %s
-			JOIN {$wpdb->prefix}terms wt ON wt.term_id = wtt.term_id
-			WHERE wp.post_type = 'post'
-			AND wp.post_status = 'publish'
-			ORDER BY wp.ID;
-SQL;
-		// phpcs:ignore -- false positive, all params are fully sanitized.
-		$results_post_ids               = $wpdb->get_results( $wpdb->prepare( $sql_get_post_ids_with_taxonomy, $tag_taxonomy ), ARRAY_A );
-
-		if ( ! empty( $results_post_ids ) ) {
-			foreach ( $results_post_ids as $result_post_id ) {
-				$post_ids[] = $result_post_id['ID'];
-			}
-		}
-
-		return $post_ids;
-	}
-
-	/**
-	 * For a post ID, gets tags which have the given taxonomy.
-	 *
-	 * @param int    $post_id         Post ID.
-	 * @param string $tag_taxonomy Tag tagxonomy.
-	 *
-	 * @return array Tag names with given taxonomy which this post has.
-	 */
-	public function get_post_tags_with_taxonomy( $post_id, $tag_taxonomy ) {
-		global $wpdb;
-		$names = [];
-
-		// TODO: switch to WP_Query instead of raw SQL ( e.g. if ( ! taxonomy_exists( $tag ) ) register_taxonomy( $tag, $object_type ) ).
-		$sql_get_post_ids_with_taxonomy = <<<SQL
-			SELECT DISTINCT wt.name
-			FROM {$wpdb->prefix}terms wt
-			JOIN {$wpdb->prefix}term_taxonomy wtt ON wtt.taxonomy = %s AND wtt.term_id = wt.term_id
-			JOIN {$wpdb->prefix}term_relationships wtr ON wtt.term_taxonomy_id = wtr.term_taxonomy_id
-			JOIN {$wpdb->prefix}posts wp ON wp.ID = wtr.object_id AND wp.ID = %d
-			WHERE wp.post_type = 'post'
-			AND wp.post_status = 'publish'
-SQL;
-		// phpcs:ignore -- false positive, all params are fully sanitized.
-		$results_names                  = $wpdb->get_results( $wpdb->prepare( $sql_get_post_ids_with_taxonomy, $tag_taxonomy, $post_id ), ARRAY_A );
-		if ( ! empty( $results_names ) ) {
-			foreach ( $results_names as $results_name ) {
-				$names[] = $results_name['name'];
-
-			}
-		}
-
-		return $names;
 	}
 
 	/**
