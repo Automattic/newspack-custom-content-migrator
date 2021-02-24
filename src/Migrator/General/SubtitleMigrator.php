2fd7770791388a1a6264833bc42c661a821bd124
Philip John <phil@philipjohn.me.uk>
2021-02-24 14:56:14 +0000

    fix: actually skip when there's an empty subtitle

--- src/Migrator/General/SubtitleMigrator.php
+++ src/Migrator/General/SubtitleMigrator.php
@@ -152,6 +152,7 @@ class SubtitleMigrator implements InterfaceMigrator {
 			$subtitle          = get_post_meta( $post->ID, esc_sql( $meta_key ), true );
 			if ( empty( $subtitle ) ) {
 				WP_CLI::warning( sprintf( 'Existing subtitle on %d is empty. Skipping.', $post->ID ) );
+				continue;
 			}
 
 			$newspack_subtitle = ( ! $dry_run ) ? update_post_meta( $post->ID, 'newspack_post_subtitle', esc_sql( $subtitle ) ) : true;
