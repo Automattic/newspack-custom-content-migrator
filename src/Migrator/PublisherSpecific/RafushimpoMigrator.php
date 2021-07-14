4f94a1b7e188523022b9e729fe8aeb5f8e6d3a07
Ivan Uravic <iuravic@gmail.com>
2021-07-14 16:37:35 +0200

    V2

--- src/Migrator/PublisherSpecific/RafushimpoMigrator.php
+++ src/Migrator/PublisherSpecific/RafushimpoMigrator.php
@@ -9,11 +9,17 @@ use \NewspackCustomContentMigrator\MigrationLogic\Attachments as AttachmentsLogi
 use Symfony\Component\DomCrawler\Crawler as Crawler;
 use \WP_CLI;
 use \WP_Query;
+use \WP_CLI\Utils;
 
 /**
  * Custom migration scripts for Rafu Shimpo.
  */
 class RafushimpoMigrator implements InterfaceMigrator {
+
+	/**
+	 * Post Meta telling us what the old Post ID was.
+	 */
+	CONST META_OLD_ID = '_newspack_old_post_id';
 
 	/**
 	 * @var PostsLogic.
@@ -97,6 +103,15 @@ class RafushimpoMigrator implements InterfaceMigrator {
 						'optional'    => false,
 						'repeating'   => false,
 					],
+				],
+			]
+		);
+		WP_CLI::add_command(
+			'newspack-content-migrator rafushimpo-output-sql-to-add-meta-with-current-id-to-posts-and-pages',
+			[ $this, 'cmd_output_sql_to_add_meta_with_current_id_to_posts_and_pages' ],
+			[
+				'shortdesc' => 'Outputs on screen the SQL query which adds meta to all Posts and Pages containing the current ID as its value.',
+				'synopsis'  => [
 				],
 			]
 		);
@@ -187,47 +202,174 @@ class RafushimpoMigrator implements InterfaceMigrator {
 	 * @param array $assoc_args CLI associative arguments.
 	 */
 	public function cmd_convert_obituaries_from_posts_to_pages( $args, $assoc_args ) {
-		$time_start = microtime( true );
-
 		$file = $assoc_args[ 'file-slugs' ] ?? null;
 		if ( ! file_exists( $file ) ) {
 			WP_CLI::error( sprintf( 'File %s not found.', $file ) );
 		}
 
-
-// 		$line = '/2016/12/frances-tsuyuko-nishibayashi';
-// 		$page = get_page_by_path( $line, OBJECT, 'page' );
-// exit;
-// 		$line = '/2017/04/村上園';
-// 		$line = '/?page_id=2722';
-
-// WILL '/?page_id=2722' still be the same ID ???
-
+		global $wpdb;
+		$time_start = microtime( true );
 		$pages_all = explode( "\n", file_get_contents( $file ) );
-		$pages_not_found = [];
+		$progress = Utils\make_progress_bar( 'Examining links...', count( $pages_all ) );
+		$pages_converted = [];
+		$pages_converted_error = [];
+		$links_not_found = [];
 
 		$handle = fopen( $file, 'r' );
 		if ( $handle ) {
 			while ( ( $line = fgets( $handle ) ) !== false ) {
-				$line = str_replace( [ "\n", "\r" ], '', $line );
-				$line = rtrim( $line, '/' );
-// $line = $line . 'asdfsa';
+				$progress->tick();
 
-				// $page = get_page_by_path( $line, OBJECT, 'page' );
-				// if ( null == $page ) {
-				$page_id = url_to_postid( $line );
+				$line = str_replace( [ "\n", "\r" ], '', $line );
+				$line = trim( $line, ' ' );
+				$line = rtrim( $line, '/' );
+
+				// Using 0 as unset for $page_id, because that's what `url_to_postid()` returns on no match. Using null for all other custom vars.
+				$page_id = 0;
+				$original_page_id = null;
+
+				// First we should try and get the original Post/Page ID from the URL, and only use WP's functions to get the ID if the old ID isn't present in the URL.
+				// $pages_id is always 0 here, just making an obvious logical point... :)
 				if ( 0 == $page_id ) {
-					$pages_not_found[] = $line;
+
+					// Get original ID from query params.
+					$parsed_line = parse_url( $line );
+					$query_params = [];
+					parse_str( $parsed_line[ 'query' ] ?? '', $query_params );
+					if ( isset( $parsed_line[ 'query' ] ) && isset( $query_params[ 'p' ] ) && ! empty( $query_params[ 'p' ] ) ) {
+						$original_page_id = $query_params[ 'p' ];
+					}
+					if ( isset( $parsed_line[ 'query' ] ) && isset( $query_params[ 'post' ] ) && ! empty( $query_params[ 'post' ] ) ) {
+						$original_page_id = $query_params[ 'post' ];
+					}
+					if ( isset( $parsed_line[ 'query' ] ) && isset( $query_params[ 'page_id' ] ) && ! empty( $query_params[ 'page_id' ] ) ) {
+						$original_page_id = $query_params[ 'page_id' ];
+					}
+
+					// Also try and get the original ID from their previous permalink format which ended in the numeric ID.
+					if ( null == $original_page_id ) {
+						$line_trimmed = rtrim( $line, '/' );
+						$line_exploded = explode( '/', $line_trimmed );
+						if ( is_numeric( $line_exploded[ count( $line_exploded ) - 1 ] ) ) {
+							$original_page_id = $line_exploded[ count( $line_exploded ) - 1 ];
+						}
+					}
+
+					// If we matched the old Post ID, now let's get the current Post ID from the meta.
+					if ( ! is_null( $original_page_id ) ) {
+						$res_original_post_id = $wpdb->get_row( $wpdb->prepare( "select post_id from {$wpdb->postmeta} where meta_key = %s and meta_value = %d ;", self::META_OLD_ID, $original_page_id ), ARRAY_A );
+						if ( isset( $res_original_post_id[ 'post_id' ] ) ) {
+							$page_id = $res_original_post_id[ 'post_id' ];
+						}
+					}
 				}
-$d=1;
+
+				// Only if the ID is still not found by using the manual ways, try the native ways to get the current ID from the URL.
+				if ( 0 == $page_id ) {
+					$page_id = url_to_postid( $line );
+				}
+
+				// If all else fails, try and get the Post from the last URL segment as the Post title.
+				if ( 0 == $page_id ) {
+					$line_exploded = explode( '/', $line );
+					$last_segment = $line_exploded[ count( $line_exploded ) - 1 ];
+					if ( ! empty( $last_segment ) ) {
+						$last_segment_with_spaces = str_replace( '-', ' ', $last_segment );
+						$row = $wpdb->get_row( $wpdb->prepare( "select ID from {$wpdb->posts} where post_title = %s ;", $last_segment_with_spaces ), ARRAY_A );
+						if ( isset( $row[ 'ID' ] ) ) {
+							$page_id = $row[ 'ID' ];
+						}
+					}
+				}
+
+				if ( 0 == $page_id ) {
+					$links_not_found[] = $line;
+				} else {
+					$pages_converted[ $page_id ] = $line;
+				}
 			}
 
 			fclose($handle);
+			$progress->finish();
 		} else {
 			WP_CLI::error( sprintf( 'Error opening file %s.', $file ) );
 		}
 
+
+		// Convert obituaries to Posts in the Obituaries category.
+		WP_CLI::line( sprintf( 'Now converting %d Pages to Posts in the `Obituaries` Category...', count( $pages_converted ) ) );
+		$obituaries_category_id = wp_create_category( 'Obituaries' );
+		$key_pages_converted = 0;
+		foreach ( $pages_converted as $page_id => $link ) {
+
+			$key_pages_converted++;
+			WP_CLI::line( sprintf( '(%d/%d) updating %s %s', $key_pages_converted, count( $pages_converted ), $page_id, $link ) );
+
+			$page = get_post( $page_id );
+			if ( null == $page ) {
+				WP_CLI::warning( sprintf( 'Error updating ID %d link %s', $page_id, $link ) );
+				unset( $pages_converted[ $page_id ] );
+				$pages_converted_error[ $page_id ] = $link;
+				continue;
+			}
+
+			$wpdb->update( $wpdb->prefix . 'posts', [ 'post_type' => 'post' ], [ 'ID' => $page->ID ] );
+			wp_set_post_categories( $page->ID, $obituaries_category_id, true );
+
+		}
+
+
+		// Log and report.
+		if ( ! empty( $links_not_found ) ) {
+			$file_links_not_found = substr( $file, 0, strpos( $file, '.' ) ) . '___notFound' . substr( $file, strpos( $file, '.' ) );
+			file_put_contents( $file_links_not_found, implode( "\n", $links_not_found ) );
+			WP_CLI::warning( sprintf( '%d/%d links were not found from the list you provided -- logged to %s', count( $links_not_found ), count( $pages_all ), $file_links_not_found ) );
+		}
+
+		$file_pages_converted = substr( $file, 0, strpos( $file, '.' ) ) . '___pagesConverted' . substr( $file, strpos( $file, '.' ) );
+		$pages_converted_imploded = '';
+		foreach ( $pages_converted as $page_id => $link ) {
+			$pages_converted_imploded .= ! empty( $pages_converted ) ? "\n" : '';
+			$pages_converted_imploded .= sprintf( '%d %s', $page_id, $link );
+		}
+		file_put_contents( $file_pages_converted, $pages_converted_imploded );
+		WP_CLI::warning( sprintf( '%d/%d pages converted -- logged to %s', count( $pages_converted ), count( $pages_all ), $file_pages_converted ) );
+
+		$file_pages_converted_error = substr( $file, 0, strpos( $file, '.' ) ) . '___convertedError' . substr( $file, strpos( $file, '.' ) );
+		$pages_converted_error_imploded = '';
+		foreach ( $pages_converted_error as $page_id => $link ) {
+			$pages_converted_error_imploded .= empty( $pages_converted_error ) ? '' : "\n";
+			$pages_converted_error_imploded .= sprintf( '%d %s', $page_id, $link );
+		}
+		file_put_contents( $file_pages_converted_error, $pages_converted_error_imploded );
+		WP_CLI::warning( sprintf( '%d/%d pages not updated successfully -- logged to %s', count( $pages_converted_error ), count( $pages_all ), $file_pages_converted_error ) );
+
+
+		wp_cache_flush();
+
 		WP_CLI::line( sprintf( 'All done! 🙌 Took %d mins.', floor( ( microtime( true ) - $time_start ) / 60 ) ) );
+	}
+
+	/**
+	 * Callable for the `newspack-content-migrator rafushimpo-output-sql-to-add-meta-with-current-id-to-posts-and-pages` command.
+	 *
+	 * @param array $args       CLI arguments.
+	 * @param array $assoc_args CLI associative arguments.
+	 */
+	public function cmd_output_sql_to_add_meta_with_current_id_to_posts_and_pages( $args, $assoc_args ) {
+		$meta_key = self::META_OLD_ID;
+		$query = <<<SQL
+INSERT INTO wp_postmeta ( post_id, meta_key, meta_value )
+SELECT ID, '{$meta_key}', ID
+FROM wp_posts
+LEFT JOIN wp_postmeta ON (
+    wp_postmeta.post_id = wp_posts.ID
+    AND wp_postmeta.meta_key = '{$meta_key}'
+)
+WHERE wp_posts.post_type IN ( 'post', 'page' )
+AND wp_postmeta.meta_key IS NULL;
+SQL;
+		WP_CLI::line( $query );
 	}
 
 	/**
