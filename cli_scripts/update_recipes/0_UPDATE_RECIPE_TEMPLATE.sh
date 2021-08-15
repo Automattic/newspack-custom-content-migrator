7714843e09c69980286b1e5f0abf5917e62d7376
Ivan Uravic <iuravic@gmail.com>
2021-08-15 16:58:39 +0200

    Better wording

--- cli_scripts/update_recipes/0_UPDATE_RECIPE_TEMPLATE.sh
+++ cli_scripts/update_recipes/0_UPDATE_RECIPE_TEMPLATE.sh
@@ -17,7 +17,7 @@ LIVE_SQL_DUMP_HOSTNAME_REPLACEMENTS=(
   # [publisher.com]=publisher-launch.newspackstaging.com
   # [www.publisher.com]=publisher-launch.newspackstaging.com
 )
-# Staging's hostname -- site from which this site was cloned, e.g. "publisher-staging.newspackstaging.com".
+# Staging site hostname -- site from which this site was cloned, e.g. "publisher-staging.newspackstaging.com"
 STAGING_SITE_HOSTNAME=""
 # Leave this empty in most case. In rare cases where the Live site uses a different table prefix than this local site, set the Live prefix here.
 JETPACK_TABLE_PREFIX=""
@@ -33,17 +33,17 @@ TIME_START=`date +%s`
 set_config
 validate_all_params
 
-echo_ts 'purging temp folder...'
+echo_ts "purging temp dir $TEMP_DIR ..."
 purge_temp_folders
 
 # --- prepare:
 
 get_vip_search_replace
 
-echo_ts 'starting to unpack the Jetpack Rewind archive and prepare contents for import...'
+echo_ts 'unpacking Jetpack Rewind backup archive and preparing contents for import...'
 unpack_jetpack_archive
 
-echo_ts "checking $THIS_PLUGINS_NAME plugin's status..."
+echo_ts "checking $THIS_PLUGINS_NAME plugin status..."
 update_plugin_status
 
 echo_ts "backing up current DB to ${TEMP_DIR}/${DB_NAME_LOCAL}_backup_${DB_DEFAULT_CHARSET}.sql..."
@@ -51,13 +51,13 @@ dump_db ${TEMP_DIR}/${DB_NAME_LOCAL}_backup_${DB_DEFAULT_CHARSET}.sql
 
 # --- export:
 
-echo_ts 'exporting Staging site pages...'
+echo_ts 'exporting Staging site Pages...'
 wp_cli newspack-content-migrator export-all-staging-pages --output-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts 'exporting Staging site menus...'
+echo_ts 'exporting Staging site Menus...'
 wp_cli newspack-content-migrator export-menus --output-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts "exporting Staging site active theme custom CSS..."
+echo_ts "exporting Staging site custom CSS..."
 wp_cli newspack-content-migrator export-current-theme-custom-css --output-dir=$TEMP_DIR_MIGRATOR
 
 echo_ts "exporting Staging pages settings..."
@@ -66,74 +66,74 @@ wp_cli newspack-content-migrator export-pages-settings --output-dir=$TEMP_DIR_MI
 echo_ts "exporting Staging site identity settings..."
 wp_cli newspack-content-migrator export-customize-site-identity-settings --output-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts "exporting Staging site reader revenue and donation products..."
+echo_ts "exporting Staging site Reader Revenue and Donation products..."
 wp_cli newspack-content-migrator export-reader-revenue --output-dir=$TEMP_DIR_MIGRATOR
 
 echo_ts "exporting Staging site Listings..."
 wp_cli newspack-content-migrator export-listings --output-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts "exporting Staging site campaigns..."
+echo_ts "exporting Staging site Campaigns..."
 wp_cli newspack-content-migrator export-campaigns --output-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts "exporting Staging site ads..."
+echo_ts "exporting Staging site Ad units..."
 wp_cli newspack-content-migrator export-ads --output-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts "exporting Staging site newsletters..."
+echo_ts "exporting Staging site Newsletters..."
 wp_cli newspack-content-migrator export-newsletters --output-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts "exporting Reusable blocks..."
+echo_ts "exporting Reusable Blocks..."
 wp_cli newspack-content-migrator export-reusable-blocks --output-dir=$TEMP_DIR_MIGRATOR
 
 # --- import DB:
 
-echo_ts 'preparing Live site SQL dump for import...'
+echo_ts 'preparing Live SQL dump for import...'
 prepare_live_sql_dump_for_import
 
 echo_ts 'importing Live DB tables...'
 import_live_sql_dump
 
-echo_ts 'switching Staging site tables with Live site tables...'
+echo_ts 'switching Staging tables with Live tables...'
 replace_staging_tables_with_live_tables
 
 # --- import Staging site data:
 
-echo_ts 'importing all Pages from the Staging site and new pages from the Live site...'
+echo_ts 'importing Pages from Staging and Live...'
 wp_cli newspack-content-migrator import-staging-site-pages --input-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts 'importing Menus from the Staging site...'
+echo_ts 'importing Menus from Staging...'
 wp_cli newspack-content-migrator import-menus --input-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts 'importing custom CSS from the Staging site...'
+echo_ts 'importing custom CSS from Staging...'
 wp_cli newspack-content-migrator import-custom-css-file --input-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts 'importing pages settings from the Staging site...'
+echo_ts 'importing Pages settings from Staging...'
 wp_cli newspack-content-migrator import-pages-settings --input-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts 'importing identity settings from the Staging site...'
+echo_ts 'importing identity settings from Staging...'
 wp_cli newspack-content-migrator import-customize-site-identity-settings --input-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts 'importing reader revenue products from the Staging site...'
+echo_ts 'importing Reader Revenue products from Staging...'
 wp_cli newspack-content-migrator import-reader-revenue --input-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts 'importing listings from the Staging site...'
+echo_ts 'importing Listings from Staging...'
 wp_cli newspack-content-migrator import-listings --input-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts 'importing campaigns from the Staging site...'
+echo_ts 'importing Campaigns from Staging...'
 wp_cli newspack-content-migrator import-campaigns --input-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts 'importing ads from the Staging site...'
+echo_ts 'importing Ads from Staging...'
 wp_cli newspack-content-migrator import-ads --input-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts 'importing newsletters from the Staging site...'
+echo_ts 'importing Newsletters from Staging...'
 wp_cli newspack-content-migrator import-newsletters --input-dir=$TEMP_DIR_MIGRATOR
 
-echo_ts 'importing Reusable Blocks from the Staging site...'
+echo_ts 'importing Reusable Blocks from Staging...'
 wp_cli newspack-content-migrator import-reusable-blocks --input-dir=$TEMP_DIR_MIGRATOR
 
 echo_ts 'updating WooComm settings...'
 wp_cli newspack-content-migrator woocomm-setup
 
-echo_ts 'importing Staging content previously converted to blocks...'
+echo_ts 'importing content previously converted to blocks from Staging...'
 import_blocks_content_from_staging_site
 
 echo_ts 'syncing files from Live site...'
@@ -141,7 +141,7 @@ update_files_from_live_site
 
 # --- finish:
 
-echo_ts 'cleaning up options...'
+echo_ts 'cleaning up some options...'
 clean_up_options
 
 echo_ts 'updating seo settings...'
