3e677d2a063e91b28a79d922be8a3fa42defe2cd
Ivan Uravic <iuravic@gmail.com>
2021-11-26 17:40:05 +0100

    Fix test env -- use phpunit 7.* and yoast/phpunit-polyfills

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -33,5 +33,8 @@ function _manually_load_plugin() {
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// WP 5.8.2 started requiring yoast/phpunit-polyfills for tests.
+require_once dirname( __FILE__ ) . '/../vendor/autoload.php';
+
 // Start up the WP testing environment.
 require "{$_tests_dir}/includes/bootstrap.php";
