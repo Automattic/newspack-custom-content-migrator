4960f1eedf2dcdb30f4b2ce029ef9052ba70369f
Ivan Uravic <iuravic@gmail.com>
2022-02-01 16:16:58 +0100

    NJUrbanNews migrator - fix image blocks updates

--- src/Migrator/PublisherSpecific/NewJerseyUrbanNewsMigrator.php
+++ src/Migrator/PublisherSpecific/NewJerseyUrbanNewsMigrator.php
@@ -4,7 +4,6 @@ namespace NewspackCustomContentMigrator\Migrator\PublisherSpecific;
 
 use \NewspackCustomContentMigrator\Migrator\InterfaceMigrator;
 use \NewspackCustomContentMigrator\MigrationLogic\Posts as PostsLogic;
-use \NewspackContentConverter\ContentPatcher\ElementManipulators\WpBlockManipulator as WpBlockManipulator;
 use \NewspackPostImageDownloader\Downloader;
 use \Symfony\Component\DomCrawler\Crawler;
 use \WP_CLI;
@@ -25,11 +24,6 @@ class NewJerseyUrbanNewsMigrator implements InterfaceMigrator {
 	private $posts_logic;
 
 	/**
-	 * @var WpBlockManipulator.
-	 */
-	private $blockmanipulator_logic;
-
-	/**
 	 * @var Crawler.
 	 */
 	private $crawler;
@@ -39,7 +33,6 @@ class NewJerseyUrbanNewsMigrator implements InterfaceMigrator {
 	 */
 	private function __construct() {
 		$this->posts_logic = new PostsLogic();
-		$this->blockmanipulator_logic = new WpBlockManipulator();
 		$this->crawler = new Crawler();
 	}
 
@@ -76,12 +69,12 @@ class NewJerseyUrbanNewsMigrator implements InterfaceMigrator {
 
 		$posts = $this->posts_logic->get_all_posts();
 		foreach ( $posts as $key_post => $post ) {
-
 			echo sprintf( "%d/%d %d\n", $key_post+1, count( $posts ), $post->ID );
+
 			$post_content_updated = $post->post_content;
 
-			// detect all img blocks
-			$matches = $this->blockmanipulator_logic->match_wp_block( 'wp:image', $post->post_content );
+			// Detect all img blocks.
+			$matches = $this->match_wp_block( 'wp:image', $post->post_content );
 			if ( ! $matches ) {
 				continue;
 			}
@@ -89,7 +82,7 @@ class NewJerseyUrbanNewsMigrator implements InterfaceMigrator {
 			foreach ( $matches[0] as $match ) {
 				$img_block = $match[0];
 
-				// get img src
+				// Get img src.
 				$this->crawler->clear();
 				$this->crawler->add( $img_block );
 				$img_crawler = $this->crawler->filter( 'img' );
@@ -101,13 +94,13 @@ class NewJerseyUrbanNewsMigrator implements InterfaceMigrator {
 					break;
 				}
 
-				// Replace njurbannews.com img srcs to use local hostname.
+				// Replace njurbannews.com img src to use the local hostname.
 				$img_src_parsed = parse_url( $img_src );
 				if ( 'njurbannews.com' != $img_src_parsed[ 'host' ] && 'www.njurbannews.com' != $img_src_parsed[ 'host' ] ) {
 					continue;
 				}
 
-				// Get img_src using this local hostname.
+				// Get src w/ local hostname.
 				$this_hostname = parse_url( get_site_url() )[ 'host' ];
 				$img_src_this_hostname = $img_src_parsed[ 'scheme' ] . '://' . $this_hostname . $img_src_parsed[ 'path' ] . ( $img_src_parsed[ 'query' ] ?? '' );
 				echo sprintf( "  - replacing %s >> %s\n", $img_src, $img_src_this_hostname );
@@ -115,12 +108,36 @@ class NewJerseyUrbanNewsMigrator implements InterfaceMigrator {
 			}
 
 
-			// update post
+			// Update Post.
 			if ( $post->post_content != $post_content_updated ) {
 				$updated = $wpdb->update( $wpdb->posts, [ 'post_content' => $post_content_updated ], [ 'ID' => $post->ID ] );
 				echo "  + saved\n";
 			}
 		}
+	}
+
+	public function match_wp_block( $block_name, $subject ) {
+		$pattern = '|
+		\<\!--      # beginning of the block element
+		\s          # followed by a space
+		%1$s        # element name/designation, should be substituted by using sprintf(), eg. sprintf( $this_pattern, \'wp:video\' );
+		.*?         # anything in the middle
+		--\>        # end of opening tag
+		.*?         # anything in the middle
+		(\<\!--     # beginning of the closing tag
+		\s          # followed by a space
+		/           # one forward slash
+		%1$s        # element name/designation, should be substituted by using sprintf(), eg. sprintf( $this_pattern, \'wp:video\' );
+		\s          # followed by a space
+		--\>)       # end of block
+				    # "s" modifier also needed here to match accross multi-lines
+		|xims';
+
+
+		$pattern = sprintf( $pattern, $block_name );
+
+		$preg_match_all_result = preg_match_all( $pattern, $subject, $matches, PREG_OFFSET_CAPTURE );
+		return ( false === $preg_match_all_result || 0 === $preg_match_all_result ) ? null : $matches;
 	}
 
 }
