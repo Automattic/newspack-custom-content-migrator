080fe85bc5a77ce9c65083a244486da77466116a
Zakariae Filali <filali.zakariae@gmail.com>
2022-03-03 16:36:19 +0300

    style(NGG Migrator): code styling fixes

--- src/Migrator/General/NextgenGalleryMigrator.php
+++ src/Migrator/General/NextgenGalleryMigrator.php
@@ -12,11 +12,11 @@ use \NewspackContentConverter\ContentPatcher\ElementManipulators\SquareBracketsE
 class NextgenGalleryMigrator implements InterfaceMigrator {
 
 	// Mtas saved to imported images:
-	//   - original NGG picture ID;
+	// - original NGG picture ID;
 	const META_NGG_PICTUREID = '_newspack_ngg_pictureid';
-	//   - which gallery ID this image belonged to;
+	// - which gallery ID this image belonged to;
 	const META_NGG_PICTURE_GALLERYID = '_newspack_ngg_picture_galleryid';
-	//   - sort order for this image in belonging NGG Gallery;
+	// - sort order for this image in belonging NGG Gallery;
 	const META_NGG_PICTURE_SORTORDER = '_newspack_ngg_picture_sortorder';
 
 	/**
@@ -53,9 +53,9 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 	 * NextgenGalleryMigrator constructor.
 	 */
 	private function __construct() {
-		$this->downloader = new Downloader();
-		$this->posts_logic = new PostsLogic();
-		$this->wpblock_manipulator = new WpBlockManipulator();
+		$this->downloader                        = new Downloader();
+		$this->posts_logic                       = new PostsLogic();
+		$this->wpblock_manipulator               = new WpBlockManipulator();
 		$this->squarebracketselement_manipulator = new SquareBracketsElementManipulator();
 	}
 
@@ -67,8 +67,7 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 	public static function get_instance() {
 		$class = get_called_class();
 		if ( null === self::$instance ) {
-			self::$instance = new $class;
-
+			self::$instance = new $class();
 		}
 
 		return self::$instance;
@@ -96,34 +95,32 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 		global $wpdb;
 
 		// Make sure NGG DB tables are available.
-		$this->validate_db_tables_exist( [ $wpdb->prefix . 'ngg_album', $wpdb->prefix . 'ngg_gallery', $wpdb->prefix . 'ngg_pictures' ] );
+		$this->validate_db_tables_exist( array( $wpdb->prefix . 'ngg_album', $wpdb->prefix . 'ngg_gallery', $wpdb->prefix . 'ngg_pictures' ) );
 
-		$ngg_options = get_option( 'ngg_options' );
+		$ngg_options          = get_option( 'ngg_options' );
 		$this->galleries_rows = $wpdb->get_results( " select * from {$wpdb->prefix}ngg_gallery ; ", ARRAY_A );
 
 
 		echo sprintf( "CHECKING NGG IMAGE FILES...\n" );
-		$images_rows = $wpdb->get_results( " select * from {$wpdb->prefix}ngg_pictures ; ", ARRAY_A );
-		$missing_pid_files = [];
+		$images_rows       = $wpdb->get_results( " select * from {$wpdb->prefix}ngg_pictures ; ", ARRAY_A );
+		$missing_pid_files = array();
 		foreach ( $images_rows as $key_image_row => $image_row ) {
-			$gallery_row = $this->get_gallery_row_by_gid( $image_row[ 'galleryid' ] );
-			$image_file_full_path = rtrim( WP_CONTENT_DIR, '/wp-content' ) . $gallery_row[ 'path' ] . $image_row[ 'filename' ];;
+			$gallery_row = $this->get_gallery_row_by_gid( $image_row['galleryid'] );
 			if ( ! file_exists( $image_file_full_path ) ) {
 				$missing_pid_files[] =  $image_row[ 'pid' ];
 			}
 		}
 		if ( ! empty( $missing_pid_files ) ) {
-			\WP_CLI::confirm( sprintf( "Not found %d image files from total %d. Do you still want to continue importing available images, and converting NGG galleries to Gutenberg Galleries with just these available images?", count( $missing_pid_files ), count( $images_rows ) ) );
+			\WP_CLI::confirm( sprintf( 'Not found %d image files from total %d. Do you still want to continue importing available images, and converting NGG galleries to Gutenberg Galleries with just these available images?', count( $missing_pid_files ), count( $images_rows ) ) );
 		}
-
 
 		echo sprintf( "IMPORTING NGG IMAGES TO MEDIA LIBRARY...\n" );
 		foreach ( $images_rows as $key_image_row => $image_row ) {
-			echo sprintf( "%d/%d importing image `pid` %d %s\n", $key_image_row+1, count( $images_rows ), $image_row[ 'pid' ], $image_row[ 'filename' ] );
+			echo sprintf( "%d/%d importing image `pid` %d %s\n", $key_image_row + 1, count( $images_rows ), $image_row['pid'], $image_row['filename'] );
 
-			$gallery_row = $this->get_gallery_row_by_gid( $image_row[ 'galleryid' ] );
+			$gallery_row = $this->get_gallery_row_by_gid( $image_row['galleryid'] );
 			if ( is_null( $gallery_row ) ) {
-				echo sprintf( "ERROR not found gallery `gid` for image `pid` %d %s\n", $image_row[ 'pid' ] );
+				echo sprintf( "ERROR not found gallery `gid` for image `pid` %d %s\n", $image_row['pid'] );
 				continue;
 			}
 			$att_id = $this->import_ngg_image_to_media_library( $image_row, $gallery_row );
@@ -136,23 +133,18 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 			echo sprintf( "imported att_id %d\n", $att_id );
 		}
 
-
-		echo sprintf( "\nCONVERTING NEXTGEN GALLERIES TO GUTENBERG GALLERY BLOCKS...\n" );
-		$posts_ids = $this->posts_logic->get_all_posts_ids( $post_type = [ 'post', 'page' ] );
+		$posts_ids = $this->posts_logic->get_all_posts_ids( $post_type = array( 'post', 'page' ) );
 		foreach ( $posts_ids as $key_post_id => $post_id ) {
-			echo sprintf( "%d/%d post ID %s\n", $key_post_id+1, count( $posts_ids ), $post_id );
-			$post = get_post( $post_id );
-			$updated = $this->convert_ngg_galleries_in_post_to_gutenberg_gallery( $post );
+			echo sprintf( "%d/%d post ID %s\n", $key_post_id + 1, count( $posts_ids ), $post_id );
 			if ( is_wp_error( $updated ) ) {
 				echo sprintf( "%s\n", $updated->get_error_message() );
 				continue;
-			} else if ( true === $updated ) {
+			} elseif ( true === $updated ) {
 				echo sprintf( "converted to Gutenberg Gallery\n" );
 			}
 		}
 
-
-		echo sprintf( "\nDONE. Next:\n- clean up and delete all images from the NGG gallery path %s\n", $ngg_options[ 'gallerypath' ] );
+		echo sprintf( "\nDONE. Next:\n- clean up and delete all images from the NGG gallery path %s\n", $ngg_options['gallerypath'] );
 		wp_cache_flush();
 	}
 
@@ -168,24 +160,22 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 		global $wpdb;
 
 		// Check if img was already imported.
-		$att_post_id = $wpdb->get_var( $wpdb->prepare( "select post_id from {$wpdb->postmeta} where meta_key = %s and meta_value = %s ;", self::META_NGG_PICTUREID, $image_row[ 'pid' ] ) );
+		$att_post_id = $wpdb->get_var( $wpdb->prepare( "select post_id from {$wpdb->postmeta} where meta_key = %s and meta_value = %s ;", self::META_NGG_PICTUREID, $image_row['pid'] ) );
 		if ( ! is_null( $att_post_id ) ) {
-			return new \WP_Error( 200, sprintf( "skipping, ngg_image pid %d already imported as att ID %s\n", $image_row[ 'pid' ], $att_post_id ) );
+			return new \WP_Error( 200, sprintf( "skipping, ngg_image pid %d already imported as att ID %s\n", $image_row['pid'], $att_post_id ) );
 		}
 
 		// Img info.
-		$filename = $image_row[ 'filename' ];
-		$description = $image_row[ 'description' ];
-		$alt = $image_row[ 'alttext' ];
+		$filename    = $image_row['filename'];
+		$description = $image_row['description'];
+		$alt         = $image_row['alttext'];
 
 		// Gallery and path info.
-		$atomic_compatible_public_folder_path = rtrim( WP_CONTENT_DIR, '/wp-content' );
-		$image_path = $atomic_compatible_public_folder_path . $gallery_row[ 'path' ];
-		$image_file_full_path = $image_path . $filename;
+		$image_file_full_path                 = $image_path . $filename;
 
 		// Check if file exists.
 		if ( ! file_exists( $image_file_full_path ) ) {
-			return new \WP_Error( 100, sprintf( "ERROR ngg_image pid %d not found in %s\n", $image_row[ 'pid' ], $image_file_full_path ) );
+			return new \WP_Error( 100, sprintf( "ERROR ngg_image pid %d not found in %s\n", $image_row['pid'], $image_file_full_path ) );
 		}
 
 		// Import to Media Library.
@@ -201,9 +191,9 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 		}
 
 		// Save meta referencing these Attachments as NGG pictures.
-		add_post_meta( $att_id, self::META_NGG_PICTUREID, $image_row[ 'pid' ] );
-		add_post_meta( $att_id, self::META_NGG_PICTURE_GALLERYID, $image_row[ 'galleryid' ] );
-		add_post_meta( $att_id, self::META_NGG_PICTURE_SORTORDER, $image_row[ 'sortorder' ] );
+		add_post_meta( $att_id, self::META_NGG_PICTUREID, $image_row['pid'] );
+		add_post_meta( $att_id, self::META_NGG_PICTURE_GALLERYID, $image_row['galleryid'] );
+		add_post_meta( $att_id, self::META_NGG_PICTURE_SORTORDER, $image_row['sortorder'] );
 
 		return $att_id;
 	}
@@ -231,9 +221,9 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 
 		// Persist updates.
 		if ( $post->post_content != $post_content_updated ) {
-			$updated = $wpdb->update( $wpdb->posts, [ 'post_content' => $post_content_updated ], [ 'ID' => $post->ID ] );
+			$updated = $wpdb->update( $wpdb->posts, array( 'post_content' => $post_content_updated ), array( 'ID' => $post->ID ) );
 			if ( $updated != 1 || false === $updated ) {
-				return new \WP_Error( 103, sprintf( "ERR could not update post ID %d", $post->ID ) );
+				return new \WP_Error( 103, sprintf( 'ERR could not update post ID %d', $post->ID ) );
 			}
 
 			return ( $updated > 0 ) && ( false !== $updated );
@@ -255,9 +245,9 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 		global $wpdb;
 
 		foreach ( $tables as $table ) {
-			$row = $wpdb->get_row( $wpdb->prepare( "select * from information_schema.tables where table_schema = %s AND table_name = %s limit 1;", DB_NAME, $table ), ARRAY_A );
+			$row = $wpdb->get_row( $wpdb->prepare( 'select * from information_schema.tables where table_schema = %s AND table_name = %s limit 1;', DB_NAME, $table ), ARRAY_A );
 			if ( is_null( $row ) || empty( $row ) ) {
-				throw new \Exception( sprintf( "NGG table %s not found in DB.", $table ) );
+				throw new \Exception( sprintf( 'NGG table %s not found in DB.', $table ) );
 			}
 		}
 
@@ -271,7 +261,7 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 	 *      [ngg src="galleries" ids="3" display="pro_mosaic" captions_display_title="1" is_ecommerce_enabled="1"]
 	 *      <!-- /wp:imagely/nextgen-gallery -->
 	 *
-	 * @param int $post_id         Post ID.
+	 * @param int    $post_id         Post ID.
 	 * @param string $post_content HTML.
 	 *
 	 * @return string|\WP_Error Refreshed post_content with Gutenberg Gallery blocks instead of NGG, or \WP_Error on exceptions.
@@ -292,13 +282,13 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 			// Match the shortcode inside the NGG Gallery block.
 			$matches_shortcodes = $this->squarebracketselement_manipulator->match_shortcode_designations( 'ngg', $block_html );
 			if ( empty( $matches_shortcodes[0] ) ) {
-				return new \WP_Error( 103, sprintf( "ERR [ngg] shortcode not found inside a wp:imagely/nextgen-gallery block in post ID %d", $post_id ) );
+				return new \WP_Error( 103, sprintf( 'ERR [ngg] shortcode not found inside a wp:imagely/nextgen-gallery block in post ID %d', $post_id ) );
 			}
 
-			$att_ids = [];
+			$att_ids = array();
 			foreach ( $matches_shortcodes as $match_shortcode ) {
 				$shortcode_html = $match_shortcode[0];
-				$shortcode_src = $this->squarebracketselement_manipulator->get_attribute_value( 'src', $shortcode_html );
+				$shortcode_src  = $this->squarebracketselement_manipulator->get_attribute_value( 'src', $shortcode_html );
 				if ( 'galleries' != $shortcode_src ) {
 					continue;
 				}
@@ -311,7 +301,7 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 			}
 
 			if ( empty( $att_ids ) ) {
-				return new \WP_Error( 102, sprintf( "ERR not found any Att IDs for gallery_ids %s in post ID %d", implode( ',', $gallery_ids ), $post_id ) );
+				return new \WP_Error( 102, sprintf( 'ERR not found any Att IDs for gallery_ids %s in post ID %d', implode( ',', $gallery_ids ), $post_id ) );
 			}
 
 			// Replace NGG gallery block with Gutenberg Gallery block.
@@ -334,8 +324,9 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 		global $wpdb;
 
 		// Get images from Media Library belonging to this NGG gallery; order needs casting to number, because otherwise sorts as strings.
-		$att_ids = [];
-		$atts_rows = $wpdb->get_results( $wpdb->prepare(
+		$att_ids   = array();
+		$atts_rows = $wpdb->get_results(
+			$wpdb->prepare(
 				"select wpm.post_id, wpm_order.meta_value as sorting_order
 				from {$wpdb->prefix}postmeta wpm
 				left join {$wpdb->prefix}postmeta wpm_order
@@ -350,7 +341,7 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 			ARRAY_A
 		);
 		foreach ( $atts_rows as $att_row ) {
-			$att_ids[] = $att_row[ 'post_id' ];
+			$att_ids[] = $att_row['post_id'];
 		}
 
 		return $att_ids;
@@ -373,7 +364,7 @@ class NextgenGalleryMigrator implements InterfaceMigrator {
 <figure class="wp-block-gallery has-nested-images columns-default is-cropped">%s</figure>
 <!-- /wp:gallery -->
 HTML;
-		$image_block_html_sprintf = <<<HTML
+		$image_block_html_sprintf   = <<<HTML
 <!-- wp:image {"id":%s,"sizeSlug":"large","linkDestination":"none"} -->
 <figure class="wp-block-image size-large"><img src="%s" alt="" class="wp-image-%s"/><figcaption>%s</figcaption></figure>
 <!-- /wp:image -->
@@ -382,9 +373,9 @@ HTML;
 		// Get all belonging Images Blocks.
 		$images_blocks_html = '';
 		foreach ( $att_ids as $att_id ) {
-			$att_src = wp_get_attachment_url( $att_id );
-			$att_caption = wp_get_attachment_caption( $att_id );
-			$images_blocks_html .= empty( $images_blocks_html ) ? '' : "\n\n" ;
+			$att_src             = wp_get_attachment_url( $att_id );
+			$att_caption         = wp_get_attachment_caption( $att_id );
+			$images_blocks_html .= empty( $images_blocks_html ) ? '' : "\n\n";
 			$images_blocks_html .= sprintf( $image_block_html_sprintf, $att_id, $att_src, $att_id, $att_caption );
 		}
 
@@ -403,7 +394,7 @@ HTML;
 	 */
 	private function get_gallery_row_by_gid( $gid ) {
 		foreach ( $this->galleries_rows as $key_gallery_row => $gallery_row ) {
-			if ( $gid == $gallery_row[ 'gid' ] ) {
+			if ( $gid == $gallery_row['gid'] ) {
 				return $this->galleries_rows[ $key_gallery_row ];
 			}
 		}
