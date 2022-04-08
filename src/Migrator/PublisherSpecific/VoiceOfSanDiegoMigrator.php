752a5c94b16d538853d2c804d26488cc75d6ab2e
Zakariae Filali <filali.zakariae@gmail.com>
2022-04-08 15:32:56 +0300

    fix(VoSD): migrate the vosd_remove_featured_images_from_post_body method to the PostsMigrator

--- src/Migrator/PublisherSpecific/VoiceOfSanDiegoMigrator.php
+++ src/Migrator/PublisherSpecific/VoiceOfSanDiegoMigrator.php
@@ -59,15 +59,6 @@ class VoiceOfSanDiegoMigrator implements InterfaceMigrator {
 				'synopsis'  => array(),
 			)
 		);
-
-		WP_CLI::add_command(
-			'newspack-content-migrator remove-featured-images-from-post-body',
-			array( $this, 'vosd_remove_featured_images_from_post_body' ),
-			array(
-				'shortdesc' => 'Verify migrated bylines from live site.',
-				'synopsis'  => array(),
-			)
-		);
 	}
 
 	/**
@@ -104,57 +95,6 @@ class VoiceOfSanDiegoMigrator implements InterfaceMigrator {
 
 			if ( $live_author !== $staging_author ) {
 				$this->log( self::BYLINES_LOGS, sprintf( 'Live author (%s), Staging author (%s), Post (%d)', $live_author, $staging_author, $post->ID ) );
-			}
-		}
-	}
-
-	/**
-	 * Callable for `newspack-content-migrator remove-featured-images-from-post-body`.
-	 *
-	 * @param $args
-	 * @param $assoc_args
-	 */
-	public function vosd_remove_featured_images_from_post_body( $args, $assoc_args ) {
-		global $wpdb;
-
-		$posts = get_posts(
-			array(
-				'numberposts' => -1,
-				'post_type'   => 'post',
-				'post_status' => array( 'publish' ),
-			)
-		);
-
-		foreach ( $posts as $post ) {
-			if ( substr( $post->post_content, 0, strlen( '[caption' ) ) === '[caption' ) {
-				$start_with_images[] = $post->ID;
-
-				$pattern = get_shortcode_regex();
-				if ( preg_match_all( '/' . $pattern . '/s', $post->post_content, $matches ) && array_key_exists( 2, $matches ) && in_array( 'caption', $matches[2] ) ) {
-					$index = 0;
-					$count = 0;
-					// Remove the first image shortcode from the content.
-					foreach ( $matches[2] as $match ) {
-						if ( 'caption' === $match ) {
-							// Found our first image.
-							$index = $count;
-							break; // We've done enough.
-						}
-						$count++;
-					};
-					// Remove first gallery from content.
-					$content = str_replace( $matches[0][ $index ], '', $post->post_content );
-
-					if ( $content !== $post->post_content ) {
-						$wpdb->update(
-							$wpdb->prefix . 'posts',
-							array( 'post_content' => $content ),
-							array( 'ID' => $post->ID )
-						);
-
-						WP_CLI::line( sprintf( 'Updated post: %d', $post->ID ) );
-					}
-				}
 			}
 		}
 	}
