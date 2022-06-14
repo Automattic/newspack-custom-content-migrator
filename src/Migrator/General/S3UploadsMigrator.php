99c1eb544213a3b665a2bbaccbc5b21a0d42b24c
Ivan Uravic <iuravic@gmail.com>
2022-06-14 12:08:53 +0200

    Change arguments to upload just one directory at the time. This is simpler to integrate with S3uploads CLI command upload destination parameter

--- src/Migrator/General/S3UploadsMigrator.php
+++ src/Migrator/General/S3UploadsMigrator.php
@@ -8,25 +8,27 @@ use \WP_CLI;
 
 class S3UploadsMigrator implements InterfaceMigrator {
 
+	const LOG = 's3-uploads-migrator.log';
+
 	/**
-	 * Safe limit of number of files in a folder to upload at once for S3-Uploads not to crash.
+	 * Safe number of files to upload in a folder at once for S3-Uploads not to crash.
 	 */
 	const UPLOAD_FILES_LIMIT = 700;
 
 	/**
-	 * Files to ignore and not upload.
+	 * Batches of files for upload will be temporarily copied to a temp folder with this suffix, e.g. wp-content/uploads/YYYY/MM{SUFFIX}.
+	 */
+	const TMP_MONTH_FOLDER_SUFFIX = '__S3UPLOAD';
+
+	/**
+	 * Files to ignore while uploading.
 	 */
 	const IGNORE_UPLOADING_FILES = [
 		'.DS_Store',
 	];
 
 	/**
-	 * @var string $cli_s3_uploads_prefix Will contain the value from 's3-uploads-prefix' associative argument.
-	 */
-	private $cli_s3_uploads_prefix;
-
-	/**
-	 * @var bool $confirmed_first_upload Internal flag used to prompt the User to confirm the accuracy of the CLI command before running it for the first time.
+	 * @var bool $confirmed_first_upload Flag, used to prompt the User to confirm the accuracy of the CLI command before running it for the first time.
 	 */
 	private $confirmed_first_upload = false;
 
@@ -67,17 +69,17 @@ class S3UploadsMigrator implements InterfaceMigrator {
 			'synopsis'  => [
 				[
 					'type'        => 'assoc',
-					'name'        => 's3-uploads-prefix',
-					'description' => "Optional. Destination root prefix '{s3-uploads-prefix}'. Used in CLI command like this `$ wp s3-uploads upload-directory /srv/htdocs/wp-content/uploads/2022/01/ {s3-uploads-prefix}uploads/2022/01`. Leave empty if the config constant S3_UPLOADS_BUCKET contains the '/wp-content' suffix (for example `define( 'S3_UPLOADS_BUCKET\', 'name-of-bucket/wp-content' );`) and if you are providing the years folders. Or if '/wp-content' suffix isn't defined in S3_UPLOADS_BUCKET, use this prefix to match the destination in your bucket.",
-					'optional'    => true,
+					'name'        => 'directory-to-upload',
+					'description' => "Full path to a directory to be uploaded. For example, --directory-to-upload=/srv/htdocs/wp-content/uploads/2019",
+					'optional'    => false,
 					'repeating'   => false,
 				],
 				[
-					'type'        => 'positional',
-					'name'        => 'directory-to-upload',
-					'description' => "Repeating positional argument at the end of the command. Provide full paths to all the (years) directories to be uploaded. For example, '$ wp newspack-content-migrator s3uploads-upload-directories /srv/htdocs/wp-content/uploads/2019 /srv/htdocs/wp-content/uploads/2020 /srv/htdocs/wp-content/uploads/2021 /srv/htdocs/wp-content/uploads/2022",
+					'type'        => 'assoc',
+					'name'        => 's3-uploads-destination',
+					'description' => "Destination root prefix. It used in the CLI command like this, `$ wp s3-uploads upload-directory --directory-to-upload=/srv/htdocs/wp-content/uploads/2019 --s3-uploads-destination=uploads/2019`. This value depends on the S3_UPLOADS_BUCKET constant, and whether it contains the '/wp-content' suffix as root destination or not. E.g. if we're uploading the folder --directory-to-upload=/srv/htdocs/wp-content/uploads/2019, and if the constant already contains wp-content suffix `define( 'S3_UPLOADS_BUCKET\', 'name-of-bucket/wp-content' );`, then this param whould be --s3-uploads-destination=uploads/2019.",
 					'optional'    => false,
-					'repeating'   => true,
+					'repeating'   => false,
 				],
 			],
 		] );
@@ -96,25 +98,23 @@ class S3UploadsMigrator implements InterfaceMigrator {
 			WP_CLI::error( 'S3-Uploads plugin needs to be installed and active to run this command.' );
 		}
 
-		// Fetch arguments.
-		$this->cli_s3_uploads_prefix = isset( $assoc_args['s3-uploads-prefix'] ) ? $assoc_args['s3-uploads-prefix'] : null;
+		// Path to wp-content/.
+		$this->path_wp_content = WP_CONTENT_DIR;
 		if ( ! defined( 'WP_CONTENT_DIR' ) ) {
 			WP_CLI::error( 'WP_CONTENT_DIR is not defined.' );
 		}
-		$this->path_wp_content = WP_CONTENT_DIR;
-		$directories = [];
-		foreach ( $positional_args as $positional_arg ) {
-			if ( ! file_exists( $positional_arg ) || ! is_dir( $positional_arg ) ) {
-				WP_CLI::error( sprintf( "Incorrect uploads directory path %s.", $positional_arg ) );
-			}
-			$directories[] = rtrim( $positional_arg, '/' );
+
+		// Arguments.
+		$cli_s3_uploads_destination = isset( $assoc_args['s3-uploads-destination'] ) ? $assoc_args['s3-uploads-destination'] : null;
+		$directory_to_upload = $assoc_args[ 'directory-to-upload' ] ?? null;
+		if ( ! file_exists( $directory_to_upload ) || ! is_dir( $directory_to_upload ) ) {
+			WP_CLI::error( sprintf( "Incorrect uploads directory path %s.", $directory_to_upload ) );
 		}
 
-		foreach ( $directories as $directory ) {
-			$this->upload_contents_in_directory( $directory );
-		}
+		// Upload directory by directory.
+		$this->upload_contents_in_directory( $directory_to_upload, $cli_s3_uploads_destination );
 
-		WP_CLI::success( 'Done.' );
+		WP_CLI::success( 'Done. See log %s for full details.', self::LOG );
 	}
 
 	/**
@@ -134,11 +134,13 @@ class S3UploadsMigrator implements InterfaceMigrator {
 	}
 
 	/**
+	 * Splits files in directory into smaller batches and runs upload of batches.
+	 *
 	 * @param string $directory_path Full directory path to upload.
 	 *
 	 * @return void
 	 */
-	public function upload_contents_in_directory( $directory_path ) {
+	public function upload_contents_in_directory( $directory_path, $cli_s3_uploads_destination ) {
 
 		// Get list of files in $directory_path.
 		$files = scandir( $directory_path );
@@ -153,7 +155,7 @@ class S3UploadsMigrator implements InterfaceMigrator {
 			return;
 		}
 
-		// Get a safe number of files to upload in a batch.
+		// Prepare batches of files with a safe number of files to upload.
 		$i_current_batch = 0;
 		$batch = [];
 		foreach( $files as $file ) {
@@ -161,16 +163,16 @@ class S3UploadsMigrator implements InterfaceMigrator {
 
 			// If it's a directory, run this method recursively.
 			if ( is_dir( $file_path ) ) {
-				$this->upload_contents_in_directory( $file_path );
+				$cli_s3_uploads_destination_subdirectory = $directory_path . '/' . $file;
+				$this->upload_contents_in_directory( $file_path, $cli_s3_uploads_destination_subdirectory );
 			}
 
 			// Add file to batch, or upload the batch.
 			if ( 0 !== count( $batch ) && 0 === ( count( $batch ) % self::UPLOAD_FILES_LIMIT ) ) {
-
 				// Upload batch.
 				$i_current_batch++;
 				WP_CLI::log( sprintf( "  - uploading batch #%d in %s -- from %s to %s...", $i_current_batch, $directory_path, str_replace( $directory_path . '/', '', $batch[0] ), str_replace( $directory_path  . '/', '', $batch[ count( $batch ) - 1 ] ) ) );
-				$this->upload_a_batch_of_files( $batch );
+				$this->upload_a_batch_of_files( $batch, $cli_s3_uploads_destination );
 
 				// Reset the loop.
 				$batch = [];
@@ -183,31 +185,32 @@ class S3UploadsMigrator implements InterfaceMigrator {
 		if ( ! empty( $batch ) ) {
 			$i_current_batch++;
 			WP_CLI::log( sprintf( "  - uploading batch #%d in %s -- from %s to %s...", $i_current_batch, $directory_path, str_replace( $directory_path . '/', '', $batch[0] ), str_replace( $directory_path  . '/', '', $batch[ count( $batch ) - 1 ] ) ) );
-			$this->upload_a_batch_of_files( $batch );
+			$this->upload_a_batch_of_files( $batch, $cli_s3_uploads_destination );
 		}
 	}
 
 	/**
-	 * Uploads a batch of files from the same directory.
+	 * Prepares files in a batch for upload by copying them into a tmp folder.
 	 *
-	 * @param array $batch_of_files Full path to files to be uploaded.
+	 * @param array $batch_of_files             Full path to files to be uploaded.
+	 * @param array $cli_s3_uploads_destination S3-Uploads destination path.
 	 *
 	 * @return void
 	 */
-	public function upload_a_batch_of_files( $batch_of_files ) {
+	public function upload_a_batch_of_files( $batch_of_files, $cli_s3_uploads_destination ) {
 		if ( empty( $batch_of_files ) ) {
 			return;
 		}
 
-		// Get directory info.
+		// Get these files' path.
 		$first_file = $batch_of_files[0];
 		$file_pathinfo = pathinfo( $first_file );
-		$dir_name = $file_pathinfo[ 'dirname' ];
+		$file_dir_name = $file_pathinfo[ 'dirname' ];
 
-		// Prepare the tmp directory with the '__S3UPLOAD' suffix.
-		$dir_name_tmp = $dir_name . '__S3UPLOAD';
+		// Prepare the tmp directory for the batch.
+		$dir_name_tmp = $file_dir_name . self::TMP_MONTH_FOLDER_SUFFIX;
 		if ( file_exists( $dir_name_tmp ) ) {
-			unlink( $dir_name_tmp );
+			$this->delete_directory( $dir_name_tmp );
 		}
 		mkdir( $dir_name_tmp, 0777, true );
 
@@ -218,24 +221,25 @@ class S3UploadsMigrator implements InterfaceMigrator {
 			copy( $file, $file_destination );
 		}
 
-		// Upload the tmp directory.
-		$this->upload_folder_to_s3( $dir_name_tmp );
+		// Upload the tmp batch directory.
+		$this->upload_folder_to_s3( $dir_name_tmp, $cli_s3_uploads_destination );
 
-		// rm the tmp files.
-		unlink( $dir_name_tmp );
+		// Clean up and delete the tmp batch directory.
+		$this->delete_directory( $dir_name_tmp );
 	}
 
 	/**
 	 * Runs the S3-Uploads' upload-directory CLI command on a folder.
 	 *
-	 * @param string $dir Folder to upload
+	 * @param string $dir_from Folder to upload
 	 *
 	 * @return void
 	 */
-	public function upload_folder_to_s3( $dir ) {
-		$cli_command = sprintf( "wp s3-uploads upload-directory %s/ %suploads/2022/01_test", $dir, $this->cli_s3_uploads_prefix );
+	public function upload_folder_to_s3( $dir_from, $cli_s3_uploads_destination ) {
+		// CLI upload-directory command.
+		$cli_command = sprintf( "wp s3-uploads upload-directory %s/ %s", $dir_from, $cli_s3_uploads_destination );
 
-		// Prompt the user before running the command for the first time, just to double-check the format of everything.
+		// Prompt the user before running the command for the first time, as an opportunity to double-check the correct format.
 		if ( false === $this->confirmed_first_upload ) {
 			WP_CLI::log( "\nPlease confirm that this first upload-directory command uses the correct S3 destination:" );
 			WP_CLI::log( '  $ ' . $cli_command );
@@ -243,12 +247,68 @@ class S3UploadsMigrator implements InterfaceMigrator {
 			$this->confirmed_first_upload = true;
 		}
 
-		// Run the upload-directory command.
+		// Run the CLI command.
 		$options = [
 			'return'     => true,
 			'launch'     => false,
 			'exit_error' => true,
 		];
+		WP_CLI::log( sprintf( "running command: $ %s", $cli_command ) );
 		WP_CLI::runcommand( $cli_command, $options );
+
+		// Log command and list of files uploaded.
+		$this->log_command_and_files( $dir_from, $cli_command );
+	}
+
+	/**
+	 * Saves a full log of the CLI command and all files affected.
+	 *
+	 * @param string $dir_from    Temp folder which was uploaded.
+	 * @param string $cli_command CLI command.
+	 *
+	 * @return void
+	 */
+	public function log_command_and_files( $dir_from, $cli_command ) {
+		$files = scandir( $dir_from );
+		$ignore_files = array_merge( [ '.', '..', ], self::IGNORE_UPLOADING_FILES );
+		foreach ( $ignore_files as $file_ignore ) {
+			unset( $files[ array_search( $file_ignore, $files, true) ] ) ;
+		}
+		sort( $files );
+		$this->log( self::LOG, sprintf( "%s\n%s", $cli_command, implode( "\n", $files ) ) );
+	}
+
+	/**
+	 * Deletes directory and all its contents.
+	 *
+	 * @param string $dir Directory to be deleted.
+	 *
+	 * @return void
+	 */
+	function delete_directory( $dir ) {
+		$files = glob( $dir . '/*' );
+		foreach( $files as $file ) {
+			if( is_dir( $file ) ) {
+				$this->delete_directory( $file );
+			} else {
+				unlink( $file );
+			}
+		}
+		rmdir( $dir );
+	}
+
+	/**
+	 * Simple file logging.
+	 *
+	 * @param string  $file    File name or path.
+	 * @param string  $message Log message.
+	 * @param boolean $to_cli  Display the logged message in CLI.
+	 */
+	private function log( $file, $message, $to_cli = true ) {
+		$message .= "\n";
+		if ( $to_cli ) {
+			WP_CLI::line( $message );
+		}
+		file_put_contents( $file, $message, FILE_APPEND );
 	}
 }
