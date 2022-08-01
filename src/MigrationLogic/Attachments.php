b7e6342393e1570f71f0c74cdda71a58f6ebb645
Zakariae Filali <filali.zakariae@gmail.com>
2022-08-01 11:13:41 +0100

    feat(Attachments): detect broken image URLs in posts content

--- src/MigrationLogic/Attachments.php
+++ src/MigrationLogic/Attachments.php
@@ -80,4 +80,82 @@ class Attachments {
 
 		return $att_id;
 	}
+
+	/**
+	 * Return broken attachment URLs from posts.
+	 *
+	 * @param int[] $post_ids The post IDs we need to check the images in their content, if not set, the function looks for all the posts in the database.
+	 *
+	 * @return mixed[] Array of the broken URLs indexed by the post IDs.
+	 */
+	public function get_broken_attachment_urls_from_posts( $post_ids = [] ) {
+		$broken_images = [];
+
+		$posts = get_posts(
+            [
+				'posts_per_page' => -1,
+				'post_type'      => 'post',
+				'post_status'    => array( 'publish', 'future', 'draft', 'pending', 'private', 'inherit' ),
+				'post__in'       => $post_ids,
+			]
+        );
+
+		$total_posts = count( $posts );
+		foreach ( $posts as $index => $post ) {
+			WP_CLI::line( sprintf( 'Checking Post(%d/%d): %d', $index + 1, $total_posts, $post->ID ) );
+			$broken_post_images = [];
+
+			preg_match_all( '/<img[^>]+(?:src|data-orig-file)="([^">]+)"/', $post->post_content, $image_sources_match );
+
+			foreach ( $image_sources_match[1] as $image_source_match ) {
+				$image_request = wp_remote_head( $image_source_match, [ 'redirection' => 5 ] );
+
+				if ( is_wp_error( $image_request ) ) {
+					WP_CLI::warning(
+                        sprintf(
+                            'Local image ID (%s) returned an error: %s',
+                            $image_source_match,
+                            $image_request->get_error_message()
+                        )
+                    );
+				}
+
+				if ( 200 !== $image_request['response']['code'] ) {
+					// Check if the URL is managed by s3_uploads plugin.
+					if ( class_exists( S3_Uploads_Plugin::class ) ) {
+						$bucket       = \S3_Uploads\Plugin::get_instance()->get_s3_bucket();
+						$exploded_url = explode( '/', $image_source_match );
+						$filename     = end( $exploded_url );
+						$month        = prev( $exploded_url );
+						$year         = prev( $exploded_url );
+						$s3_url       = 'https://' . $bucket . ".s3.amazonaws.com/wp-content/uploads/$year/$month/$filename";
+
+						$image_request_from_s3 = wp_remote_head( $s3_url, [ 'redirection' => 5 ] );
+
+						if ( is_wp_error( $image_request_from_s3 ) ) {
+							WP_CLI::warning(
+								sprintf(
+									'Image ID (%s) from S3 returned an error: %s',
+									$image_request_from_s3,
+									$image_request->get_error_message()
+								)
+							);
+						}
+
+						if ( 200 !== $image_request_from_s3['response']['code'] ) {
+							$broken_post_images[] = $image_source_match;
+						}
+					} else {
+						$broken_post_images[] = $image_source_match;
+					}
+				}
+			}
+
+			if ( ! empty( $broken_post_images ) ) {
+				$broken_images[ $post->ID ] = $broken_post_images;
+			}
+		}
+
+		return $broken_images;
+	}
 }
