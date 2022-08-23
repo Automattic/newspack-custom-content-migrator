32fe14f5d616dc3d7aca93140bc1305956a52248
Ivan Uravic <iuravic@gmail.com>
2022-08-23 16:39:31 +0200

    Colorado Sun -- authors profiles to GA, initial untested

--- src/Migrator/PublisherSpecific/ColoradoSunMigrator.php
+++ src/Migrator/PublisherSpecific/ColoradoSunMigrator.php
@@ -5,6 +5,7 @@ namespace NewspackCustomContentMigrator\Migrator\PublisherSpecific;
 use \WP_CLI;
 use \NewspackCustomContentMigrator\Migrator\InterfaceMigrator;
 use NewspackCustomContentMigrator\MigrationLogic\Posts as PostsLogic;
+use NewspackCustomContentMigrator\MigrationLogic\Attachments as AttachmentsLogic;
 use NewspackContentConverter\ContentPatcher\ElementManipulators\HtmlElementManipulator;
 use NewspackContentConverter\ContentPatcher\ElementManipulators\WpBlockManipulator;
 use NewspackCustomContentMigrator\MigrationLogic\CoAuthorPlus as CoAuthorPlusLogic;
@@ -23,6 +24,11 @@ class ColoradoSunMigrator implements InterfaceMigrator {
 	 * @var PostsLogic
 	 */
 	private $posts_logic;
+
+	/**
+	 * @var AttachmentsLogic
+	 */
+	private $attachment_logic;
 
 	/**
 	 * @var CoAuthorPlusLogic
@@ -44,6 +50,7 @@ class ColoradoSunMigrator implements InterfaceMigrator {
 	 */
 	private function __construct() {
 		$this->posts_logic = new PostsLogic();
+		$this->attachment_logic = new AttachmentsLogic();
 		$this->coauthors_logic = new CoAuthorPlusLogic();
 		$this->wp_block_manipulator = new WpBlockManipulator();
 		$this->html_element_manipulator = new HtmlElementManipulator();
@@ -252,8 +259,253 @@ class ColoradoSunMigrator implements InterfaceMigrator {
 		wp_cache_flush();
 	}
 
-	public function cmd_migrate_authors_to_cap( $subject, $remove ) {
-		
+	/**
+	 * @param $positional_args
+	 * @param $assoc_args
+	 *
+	 * @return void
+	 */
+	public function cmd_migrate_authors_to_cap( $positional_args, $assoc_args ) {
+
+		global $wpdb;
+
+		/*
+		 * Things this command does:
+		 * for each post, there is a wp_postmeta.meta_key = 'byline'.  This is a serialized string that contains the post_id of
+		 * the author from the initial data import from the currently live site.  This ID does not match up with the current
+		 * wp_posts table on the staging site.  We need to move the authors over to CAP configuration.
+		 */
+
+		// Import live site's wp_posts and wp_postmeta.
+		// \WP_CLI::confirm( "Make sure you've imported the lates live_wp_posts and live_wp_postmeta (with properly updated hostnames) before continuing with this command." );
+
+		// Dev helper variable.
+		$distinct_types = [];
+
+		// Get all posts.
+		// $post_ids = $this->posts_logic->get_all_posts_ids();
+
+// DEV test
+$post_ids = [ 232150 ];
+
+
+		foreach ( $post_ids as $key_post_id => $post_id ) {
+
+			WP_CLI::log( sprintf( "(%d)/(%d) %d", $key_post_id + 1, count( $post_ids ), $post_id ) );
+			$post = get_post( $post_id );
+
+
+			// Get profiles/'byline' meta.
+			$byline_meta_value = $wpdb->get_var( $wpdb->prepare( "select meta_value from live_wp_postmeta where meta_key='byline' and post_id = %d;", $post_id ) );
+			$byline_meta = $byline_meta_value ? unserialize( $byline_meta_value ) : null;
+			if ( is_null( $byline_meta ) ) {
+				// LOG no authors.
+				$msg = sprintf( "ID=%d no_author_profiles", $post_id, count( $byline_meta['profiles'] ) );
+				$this->log( 'cs_authorprofiles_no_authors.log', $msg );
+				WP_CLI::log( $msg );
+				continue;
+			}
+
+
+			// LOG this post ID, and number of its 'profile' authors.
+			$this->log( 'cs_authorprofiles_saved_GAs.log', sprintf( "ID=%d number_of_authors=%d", $post_id, count( $byline_meta['profiles'] ) ) );
+			WP_CLI::log( sprintf( "%d authors", count( $byline_meta['profiles'] ) ) );
+
+
+			// Loop through this post's profiles.
+			foreach ( $byline_meta['profiles'] as $key_profile => $profile ) {
+
+				// Custom "profile type" variable.
+				$type = $profile['type'];
+
+
+				// Dev helper - check if type is known, distinct existing types are = [ 'byline', ]
+				if ( ! in_array( $type , $distinct_types ) ) {
+					$distinct_types = array_merge( $distinct_types, [ $type ] );
+					WP_CLI::log( sprintf( "new distinct profile type = %s", $type ) );
+					$debug=1;
+				}
+
+
+				// Get profile "atts" which contains "term_id" (profile author term_id) and "post_id" (profile author post_id).
+				$atts = $profile['atts'];
+				// Dev helper check if $atts has more values than just 'term_id' and 'post_id'.
+				if ( count( $atts ) > 2 || ! isset( $atts['term_id'] ) || ! isset( $atts['post_id'] ) ) {
+					$debug=1;
+				}
+
+
+				// Profile term_id.
+				$term_id = $atts['term_id'] ?? null;
+				$term_row = $wpdb->get_row( $wpdb->prepare( "select * from live_wp_terms where term_id = %d ;", $term_id ), ARRAY_A );
+				// Validate.
+				if ( is_null( $term_id ) || ! $term_row ) {
+					WP_CLI::log( sprintf( "no term data: term_id=%s term_row=%s", $term_id, print_r( $term_row, true ) ) );
+					$debug=1;
+				}
+
+
+				// Profile author post object ID.
+				$profile_post_id = $atts['post_id'] ?? null;
+				$profile_post_row = $wpdb->get_row( $wpdb->prepare( "select * from live_wp_posts where ID = %d ;", $profile_post_id ), ARRAY_A );
+				// Validate.
+				if ( is_null( $profile_post_id ) || ! $profile_post_row || ( 'profile' != $profile_post_row['post_type'] )  ) {
+					WP_CLI::log( sprintf(
+						"no live post data: profile_author_post_id=%s post_row=%s post_type=%s",
+						$profile_post_id,
+						( ! $profile_post_row ? 'null' : 'OK' ),
+						$profile_post_row['post_type'] ?? 'null'
+					) );
+					$debug=1;
+				}
+
+
+				// Get profile postmeta.
+				// --- email, twitter handle, live WP User associated to Post, live att_id of author avatar
+				$email = $wpdb->get_var( $wpdb->prepare( "select meta_value from live_wp_postmeta where meta_key = %s and post_id = %d ;", 'email', $profile_post_id ) );
+				$twitter_handle = $wpdb->get_var( $wpdb->prepare( "select meta_value from live_wp_postmeta where meta_key = %s and post_id = %d ;", 'twitter', $profile_post_id ) );
+				$live_wp_user_id = $wpdb->get_var( $wpdb->prepare( "select meta_value from live_wp_postmeta where meta_key = %s and post_id = %d ;", 'user_id', $profile_post_id ) );
+				$live_attachment_id = $wpdb->get_var( $wpdb->prepare( "select meta_value from live_wp_postmeta where meta_key = %s and post_id = %d ;", '_thumbnail_id', $profile_post_id ) );
+
+
+				// Get actual local avatar attachment ID.
+				$live_attachment_obj_row = $wpdb->get_row( $wpdb->prepare( "select * from live_wp_posts where ID = %d", $live_attachment_id ), ARRAY_A );
+				// This avatar attachment file path, will be e.g. '2022/01/outcalt_01.jpg'.
+				$live_attachment_wp_content_file_path = $wpdb->get_var( $wpdb->prepare( "select meta_value from live_wp_postmeta where meta_key = '_live_wp_attached_file' and post_id = %d;", $live_attachment_id ) );
+				// Check if local attachment exists.
+				$this_hostname = get_site_url();
+				$this_hostname_parsed = parse_url( $this_hostname );
+				$local_attachment_url = sprintf( "https://%s/wp-content/uploads/%s", $this_hostname_parsed['host'], $live_attachment_wp_content_file_path );
+				$ga_avatar_att_id = attachment_url_to_postid( $local_attachment_url );
+				if ( 0 == $ga_avatar_att_id ) {
+					// Import attachment if not exists.
+					// Get live attachment img URL.
+					$featured_img_live_url = sprintf( "https://lede-admin.coloradosun.com/wp-content/uploads/sites/15/%s", $live_attachment_wp_content_file_path );
+					$ga_avatar_att_id = $this->attachment_logic->import_external_file( $featured_img_live_url );
+
+					// Log downloading and importing avatar from their live site.
+					$this->log( 'cs_authorprofiles_downloaded_user_avatars.log', sprintf( "live_att_id=%d URL=%s post_ID=%d", $live_attachment_id, $live_attachment_wp_content_file_path, $post_id ) );
+					$debug=1;
+				}
+
+
+				// Get the actual local WP author User ID.
+				// --- first get the live site user data
+				$live_wp_user_row = $wpdb->get_row( $wpdb->prepare( "select * from live_wp_users where ID = %d", $live_wp_user_id ), ARRAY_A );
+				$live_wp_user_meta_rows = $wpdb->get_results( $wpdb->prepare( "select * from live_wp_usermeta where user_id = %d", $live_wp_user_id ), ARRAY_A );
+				// --- get the local user with same ID
+				$wp_user_id = $post->post_author;
+				$wp_user = get_user_by( 'ID', $wp_user_id );
+				if ( false == $wp_user ) {
+					// OK to skip, but dev help debug just the same.
+					$debug=1;
+				}
+				// --- if this same user exists locally, use it, otherwise insert a new WP user.
+				$linked_wp_user_id = null;
+				if ( $wp_user->user_login == $live_wp_user_row['user_login'] ) {
+					// Use existing user.
+					$linked_wp_user_id = $wp_user_id;
+				} else {
+					// Insert new user.
+					$inserted = $wpdb->insert(
+						$wpdb->users,
+						[
+							'user_login' => $live_wp_user_row['user_login'],
+							'user_pass' => $live_wp_user_row['user_pass'],
+							'user_nicename' => $live_wp_user_row['user_nicename'],
+							'user_email' => $live_wp_user_row['user_email'],
+							'user_url' => $live_wp_user_row['user_url'],
+							'user_registered' => $live_wp_user_row['user_registered'],
+							'user_activation_key' => $live_wp_user_row['user_activation_key'],
+							'user_status' => $live_wp_user_row['user_status'],
+							'display_name' => $live_wp_user_row['display_name'],
+							'spam' => $live_wp_user_row['spam'],
+							'deleted' => $live_wp_user_row['deleted'],
+						]
+					);
+					if (1 != $inserted ) {
+						$debug=1;
+						throw new \RuntimeException( sprintf( "Failed inserting user live_wp_users.ID = %d", $live_wp_user_id ) );
+					}
+					// Get newly inserted ID.
+					$inserted_wp_user_id = $wpdb->get_var( sprintf( "select ID from live_wp_users where user_login = %s ; ", $live_wp_user_row['user_login'] ) );
+					$linked_wp_user_id = $inserted_wp_user_id;
+
+					// Insert usermeta.
+					foreach ( $live_wp_user_meta_rows as $live_wp_user_meta_row ) {
+						$inserted = $wpdb->insert(
+							$wpdb->usermeta,
+							[
+								'user_id' => $inserted_wp_user_id,
+								'meta_key' => $live_wp_user_meta_row['meta_key'],
+								'meta_value' => $live_wp_user_meta_row['meta_value'],
+							]
+						);
+						if ( 1 != $inserted ) {
+							$debug=1;
+							throw new \RuntimeException( sprintf( "Failed inserting usermeta row live_wpusermeta.meta_id = %d", $live_wp_user_meta_row['meta_id'] ) );
+						}
+					}
+
+					// Log user creation.
+					$this->log( 'cs_authorprofiles_inserted_new_user.log', sprintf( "ID=%d, user_login=%s", $inserted_wp_user_id, $live_wp_user_row['user_login'] ) );
+					WP_CLI::log( sprintf( "iimported new User new_ID=%d, user_login=%s", $inserted_wp_user_id, $live_wp_user_row['user_login'] ) );
+					$debug=1;
+				}
+
+
+				// Prepare GA bio text.
+				$ga_email_link = $email ? sprintf( "<a class=\"ga_email_link\" href=\"mailto:%s\">%s</a>", $email, $email ) : '';
+				$ga_twitter_link = $twitter_handle ? sprintf( "<a class=\"newspack-ga-twitter-link\" target=\"_blank\" href=\"https://twitter.com/%s\">@%s</a>", $twitter_handle, $twitter_handle ) : '';
+				$ga_bio_connect_text = sprintf(
+					"%s%s%s",
+					// Start with email link.
+					$ga_email_link,
+					// Separator, space ' '.
+					( ( ! empty( $ga_email_link ) && ! empty( $ga_twitter_link ) ) ? ' ' : '' ),
+					// Twitter link.
+					$ga_twitter_link
+				);
+				$ga_bio = sprintf(
+					"%s%s%s",
+					// Author bio.
+					$profile_post_row['post_content'],
+					// Separator, new line break.
+					( ! empty( $ga_bio_connect_text ) ? "\n" : '' ),
+					// Connect text
+					$ga_bio_connect_text
+				);
+
+
+				// Get or create GA by name.
+				$ga_name = $profile_post_row['post_title'];
+				// Check if GA exists by author name.
+				$ga = $this->coauthors_logic->get_guest_author_by_display_name( $ga_name );
+				if ( $ga ) {
+					$guest_author_id = $ga->ID;
+				} else {
+					// Create if not exists.
+					$guest_author_id = $this->coauthors_logic->create_guest_author( [ 'display_name' => $ga_name, 'avatar' => $ga_avatar_att_id ] );
+				}
+				if ( is_null( $guest_author_id ) ) {
+					throw new \RuntimeException( sprintf( "GA with name %s not found or created.", $ga_name ) );
+				}
+
+
+				// Link this GA to the WP User (this will be repeated for every post, but OK...).
+				$linked_wp_user = get_user_by( 'ID', $linked_wp_user_id );
+				$this->coauthors_logic->link_guest_author_to_wp_user( $guest_author_id, $linked_wp_user );
+
+
+				// Merge existing GAs with this GA.
+				$existing_guest_author_ids = $this->coauthors_logic->get_posts_existing_ga_ids( $post_id );
+				$guest_author_ids = array_unique( array_merge( [ $guest_author_id ], $existing_guest_author_ids ) );
+
+
+				// Assign GAs to Post.
+				$this->coauthors_logic->assign_guest_authors_to_post( $guest_author_ids, $post_id );
+			}
+		}
 	}
 
 	public function remove_string_from_end_of_string( $subject, $remove ) {
