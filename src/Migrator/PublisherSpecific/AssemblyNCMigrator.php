0f56af431f12e95360848c69b0cc37398eb9f96a
Eddie Carrasco <eddies.code.shop@gmail.com>
2022-09-08 14:29:10 -0400

    feat: updating the command to run on all posts.

--- src/Migrator/PublisherSpecific/AssemblyNCMigrator.php
+++ src/Migrator/PublisherSpecific/AssemblyNCMigrator.php
@@ -236,98 +236,145 @@ class AssemblyNCMigrator implements InterfaceMigrator {
 	}
 
 	public function cmd_move_captions_to_figures() {
-		$post = get_post( 48 );
+		global $wpdb;
+		$posts = $wpdb->get_results( "SELECT ID, post_content FROM $wpdb->posts WHERE post_type = 'post' AND post_status = 'publish' ORDER BY ID" );
 
-		$dom = new DOMDocument();
-		$content = html_entity_decode( $post->post_content, ENT_QUOTES, 'UTF-8' );
-//		var_dump( $content );
-		@$dom->loadHTML( $content );
-//		echo "\n\n\n\n----\n\n\n\n";
-//		var_dump( utf8_decode( $this->get_inner_html( $dom->lastChild->firstChild ) ) );die();
+		$progress_bar = WP_CLI\Utils\make_progress_bar( 'Processing posts...', count( $posts ) );
+		foreach ( $posts as $post ) {
+			$dom     = new DOMDocument();
+			$content = html_entity_decode( $post->post_content, ENT_QUOTES, 'UTF-8' );
+			@$dom->loadHTML( $content );
 
-		$seen_header_at_top = false;
-		$seen_other_elements = false;
-		$figure = null;
-		$figure_id = null;
+			$seen_header_at_top  = false;
+			$seen_other_elements = false;
+			$figure              = null;
+			$figure_id           = null;
 
-		$remove_nodes = [];
-		foreach ( $dom->lastChild->firstChild->childNodes as $child_node ) {
-			/* @var DOMNode $child_node */
-			if ( '#comment' === $child_node->nodeName ) {
-				continue;
-			}
+			$remove_nodes = [];
+			foreach ( $dom->lastChild->firstChild->childNodes as $child_node ) {
+				/* @var DOMNode $child_node */
+				if ( '#comment' === $child_node->nodeName ) {
+					continue;
+				}
 
-			var_dump( [ $child_node->nodeName, 'seen_other_elements' => $seen_other_elements, 'seen_header_at_top' => $seen_header_at_top ]);
+				if ( ! $seen_other_elements ) {
+					if ( 'h5' !== $child_node->nodeName ) {
+						$seen_other_elements = true;
+					}
+				}
 
-			if ( ! $seen_other_elements ) {
-				if ( 'h5' !== $child_node->nodeName ) {
+				if ( ! $seen_other_elements && 'h5' === $child_node->nodeName && ! $seen_header_at_top ) {
+					// This header needs to be added as a caption to the featured image.
+					$attachment_id = get_metadata( 'post', $post->ID, '_thumbnail_id', true );
+					wp_update_post(
+						[
+							'ID'           => $attachment_id,
+							'post_excerpt' => $child_node->nodeValue,
+						]
+					);
+
+					if ( isset( $child_node->nextSibling->nextSibling ) ) {
+						$remove_nodes[] = $child_node->nextSibling->nextSibling;
+					}
+
+					if ( isset( $child_node->nextSibling ) ) {
+						$remove_nodes[] = $child_node->nextSibling;
+					}
+
+					$remove_nodes[] = $child_node;
+
 					$seen_other_elements = true;
+					$seen_header_at_top  = true;
+					continue;
+				}
+
+				if ( 'figure' === $child_node->nodeName ) {
+					if ( '#comment' === $child_node->previousSibling->previousSibling->nodeName ) {
+						$tag_string = $child_node->previousSibling->previousSibling->nodeValue;
+						$substring  = trim( substr( $tag_string, stripos( $tag_string, 'wp:image' ) + 8 ) );
+
+						$data      = json_decode( $substring, true );
+						$figure    = $child_node;
+						$figure_id = $data['id'];
+
+						if ( $figure->hasChildNodes() ) {
+							$has_fig_caption = false;
+							foreach ( $figure->childNodes as $node ) {
+								if ( 'figcaption' === $node->nodeName ) {
+									if ( $has_fig_caption ) {
+										$remove_nodes[] = $node;
+									} else {
+										$has_fig_caption = true;
+									}
+								}
+							}
+						}
+					}
+				}
+
+				if ( 'h5' === $child_node->nodeName && ! is_null( $figure ) ) {
+					wp_update_post(
+						[
+							'ID'           => $figure_id,
+							'post_excerpt' => $child_node->nodeValue,
+						]
+					);
+
+					$fragment = $dom->createDocumentFragment();
+					$fragment->appendXML( '<figcaption>' . $this->get_inner_html( $child_node ) . '</figcaption>' );
+
+					$has_fig_caption = false;
+					if ( $figure->hasChildNodes() ) {
+						foreach ( $figure->childNodes as $node ) {
+							if ( 'figcaption' === $node->nodeName ) {
+								$has_fig_caption = true;
+								break;
+							}
+						}
+					}
+
+					if ( ! $has_fig_caption ) {
+						$figure->appendChild( $fragment );
+					}
+
+					if ( isset( $child_node->previousSibling->previousSibling ) ) {
+						$remove_nodes[] = $child_node->previousSibling->previousSibling;
+					}
+
+					if ( isset( $child_node->previousSibling ) ) {
+						$remove_nodes[] = $child_node->previousSibling;
+					}
+
+					if ( isset( $child_node->nextSibling->nextSibling ) ) {
+						$remove_nodes[] = $child_node->nextSibling->nextSibling;
+					}
+
+					if ( isset( $child_node->nextSibling ) ) {
+						$remove_nodes[] = $child_node->nextSibling;
+					}
+
+					$remove_nodes[] = $child_node;
+					$figure_id      = null;
+					$figure         = null;
 				}
 			}
 
-			if ( ! $seen_other_elements && 'h5' === $child_node->nodeName && ! $seen_header_at_top ) {
-				// This header needs to be added as a caption to the featured image.
-				$attachment_id = get_metadata( 'post', 48, '_thumbnail_id', true );
-				wp_update_post(
-					[
-						'ID' => $attachment_id,
-						'post_excerpt' => $child_node->nodeValue,
-					]
-				);
-				//Need to remove comments before and after also.
-
-				$remove_nodes[] = $child_node->nextSibling->nextSibling;
-				$remove_nodes[] = $child_node->nextSibling;
-				$remove_nodes[] = $child_node;
-
-				$seen_other_elements = true;
-				$seen_header_at_top = true;
-				continue;
-			}
-
-			if ( 'figure' === $child_node->nodeName ) {
-				if ( '#comment' === $child_node->previousSibling->previousSibling->nodeName ) {
-					$tag_string = $child_node->previousSibling->previousSibling->nodeValue;
-					$substring = trim( substr( $tag_string, stripos( $tag_string, 'wp:image' ) + 8 ) );
-
-					$data = json_decode( $substring, true );
-					$figure = $child_node;
-					$figure_id = $data['id'];
+			foreach ( $remove_nodes as $node ) {
+				if ( isset( $node->parentNode ) ) {
+					$node->parentNode->removeChild( $node );
 				}
 			}
 
-			if ( 'h5' === $child_node->nodeName && ! is_null( $figure ) ) {
-				wp_update_post(
-					[
-						'ID' => $figure_id,
-						'post_excerpt' => $child_node->nodeValue,
-					]
-				);
+			wp_update_post(
+				[
+					'ID'           => $post->ID,
+					'post_content' => utf8_decode( $this->get_inner_html( $dom->lastChild->firstChild ) ),
+				]
+			);
 
-				$fragment = $dom->createDocumentFragment();
-				$fragment->appendXML( "<figcaption>$child_node->nodeValue</figcaption>" );
-				$figure->appendChild( $fragment );
-
-				$remove_nodes[] = $child_node->previousSibling->previousSibling;
-				$remove_nodes[] = $child_node->previousSibling;
-				$remove_nodes[] = $child_node->nextSibling->nextSibling;
-				$remove_nodes[] = $child_node->nextSibling;
-				$remove_nodes[] = $child_node;
-				$figure_id = null;
-				$figure = null;
-			}
+			$progress_bar->tick();
 		}
-
-		foreach ( $remove_nodes as $node ) {
-			$node->parentNode->removeChild( $node );
-		}
-
-		wp_update_post(
-			[
-				'ID' => 48,
-				'post_content' => utf8_decode( $this->get_inner_html( $dom->lastChild->firstChild ) ),
-			]
-		);
+		$progress_bar->finish();
 	}
 
 	/**
