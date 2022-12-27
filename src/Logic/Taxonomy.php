457db6df4fdd96de4a01c748ff98102c5245ca55
Ivan Uravic <iuravic@gmail.com>
2022-12-27 10:50:21 +0100

    Fix and new command

---
+++ src/Logic/Taxonomy.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Logic for working Taxonomies.
+ *
+ * @package NewspackCustomContentMigrator
+ */
+
+namespace NewspackCustomContentMigrator\Logic;
+
+use WP_CLI;
+
+/**
+ * Taxonomy implements common migration logic that are used to work with the Simple Local Avatars plugin
+ */
+class Taxonomy {
+
+	/**
+	 * Fixes counts for taxonomy.
+	 *
+	 * @param string $taxonomy Taxonomy, e.g. 'category'.
+	 *
+	 * @return void
+	 */
+	public function fix_taxonomy_term_counts( string $taxonomy ) {
+		$get_terms_args = [
+			'taxonomy'   => $taxonomy,
+			'fields'     => 'ids',
+			'hide_empty' => false,
+		];
+
+		$update_term_ids = get_terms( $get_terms_args );
+		foreach ( $update_term_ids as $key_term_id => $term_id ) {
+			wp_update_term_count_now( [ $term_id ], $taxonomy );
+		}
+
+		wp_cache_flush();
+	}
+}
