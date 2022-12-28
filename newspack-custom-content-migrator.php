2e1c26c695f0f96de54fe7c2e0754bb6ef2b59a7
Ivan Uravic <iuravic@gmail.com>
2022-12-28 16:43:07 +0100

    Fix for Content diff refresh if no file with modified IDs is used

--- newspack-custom-content-migrator.php
+++ newspack-custom-content-migrator.php
@@ -5,7 +5,7 @@
  * Plugin URI:  https://newspack.blog/
  * Author:      Automattic
  * Author URI:  https://newspack.blog/
- * Version:     1.2.0
+ * Version:     1.2.1
  *
  * @package  Newspack_Custom_Content_Migrator
  */
