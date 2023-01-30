ddc282392e96417a607dc5bcb877d01c3b0f5231
Ivan Uravic <iuravic@gmail.com>
2023-01-30 09:39:25 +0100

    Get Bethesda's GAs

--- src/Command/PublisherSpecific/Moco360Migrator.php
+++ src/Command/PublisherSpecific/Moco360Migrator.php
@@ -4,6 +4,7 @@ namespace NewspackCustomContentMigrator\Command\PublisherSpecific;
 
 use \NewspackCustomContentMigrator\Command\InterfaceCommand;
 use \NewspackCustomContentMigrator\Logic\CoAuthorPlus;
+use \NewspackCustomContentMigrator\Logic\Posts as PostsLogic;
 use \WP_CLI;
 
 /**
@@ -22,10 +23,16 @@ class Moco360Migrator implements InterfaceCommand {
 	private $coauthorsplus_logic;
 
 	/**
+	 * @var PostsLogic $posts_logic
+	 */
+	private $posts_logic;
+
+	/**
 	 * Constructor.
 	 */
 	private function __construct() {
 		$this->coauthorsplus_logic = new CoAuthorPlus();
+		$this->posts_logic = new PostsLogic();
 	}
 
 	/**
@@ -62,12 +69,38 @@ class Moco360Migrator implements InterfaceCommand {
 	 */
 	public function cmd_bethesda_get_gas_and_their_posts( $args, $assoc_args ) {
 		// Get all GAs.
-		// Info like name, bio, ...
-		// Is linked to WP User account? Which.
+		WP_CLI::log( 'Getting all GAs...' );
+		$gas = $this->coauthorsplus_logic->get_all_gas();
+		$gas_arr = [];
+		foreach ( $gas as $ga ) {
+			$gas_arr[] = (array) $ga;
+		}
+		// If GA is linked to WP User account is stored in 'linked_account'.
 
 		// Save GA authors info file.
+		$gas_file = 'bethesda_gas.json';
+		file_put_contents( $gas_file, json_encode( $gas_arr) );
+		WP_CLI::success( sprintf( 'Saved %s', $gas_file ) );
 
 		// Get all Posts' GAs and WP User authors.
+		WP_CLI::log( 'Getting Post IDs...' );
+		$post_ids = $this->posts_logic->get_all_posts_ids();
+		$post_ids_ga_ids = [];
+		foreach ( $post_ids as $key_post_id => $post_id ) {
+			WP_CLI::log( sprintf( '(%d)/(%d) %d', $key_post_id + 1, count( $post_ids ), $post_id ) );
+			$ga_ids = $this->coauthorsplus_logic->get_posts_existing_ga_ids( $post_id );
+			if ( ! empty( $ga_ids ) ) {
+				$post_ids_ga_ids[] = [
+					'post_id' => $post_id,
+					'ga_ids' => $ga_ids,
+				];
+			}
+		}
+
+		// Save Post IDs' GAs.
+		$postids_gaids_file = 'bethesda_postids_gaids.json';
+		file_put_contents( $postids_gaids_file, json_encode( $post_ids_ga_ids) );
+		WP_CLI::success( sprintf( 'Saved %s', $postids_gaids_file ) );
 	}
 
 	/**
