9e313976b602ddb4c8e13168ca923d656de1c92a
Ivan Uravic <iuravic@gmail.com>
2023-02-20 15:40:18 +0100

    Migrate Liveblogs

--- src/Command/PublisherSpecific/VTDiggerMigrator.php
+++ src/Command/PublisherSpecific/VTDiggerMigrator.php
@@ -12,6 +12,9 @@ use \WP_CLI;
 class VTDiggerMigrator implements InterfaceCommand {
 
 	const NEWS_BRIEFS_CAT_NAME = 'News Briefs';
+	const LIVEBLOGS_CAT_NAME = 'Liveblogs';
+
+	// Tells us which CPT this post was originally, e.g. 'liveblog'.
 	const META_VTD_CPT = 'newspack_vtd_cpt';
 
 	/**
@@ -57,6 +60,14 @@ class VTDiggerMigrator implements InterfaceCommand {
 				'synopsis'  => [],
 			]
 		);
+		WP_CLI::add_command(
+			'newspack-content-migrator vtdigger-migrate-liveblogs',
+			[ $this, 'cmd_liveblogs' ],
+			[
+				'shortdesc' => 'Migrates the Liveblog CPT to regular posts with category.',
+				'synopsis'  => [],
+			]
+		);
 	}
 
 	/**
@@ -69,6 +80,7 @@ class VTDiggerMigrator implements InterfaceCommand {
 	 */
 	public function cmd_newsbriefs( array $pos_args, array $assoc_args ) {
 		global $wpdb;
+		$log = 'vtd_newsbriefs.log';
 
 		// Get News Briefs category ID.
 		$newsbriefs_cat_id = get_cat_ID( self::NEWS_BRIEFS_CAT_NAME );
@@ -89,7 +101,7 @@ class VTDiggerMigrator implements InterfaceCommand {
 			update_post_meta( $newsbrief_id, self::META_VTD_CPT, 'news-brief' );
 		}
 
-		$this->logger->log( 'vtd_newsbriefs.log', implode( ',', $newsbriefs_ids ), false );
+		$this->logger->log( $log, implode( ',', $newsbriefs_ids ), false );
 		wp_cache_flush();
 
 		// Assign category 'News Briefs'.
@@ -99,6 +111,50 @@ class VTDiggerMigrator implements InterfaceCommand {
 		}
 
 		wp_cache_flush();
-		WP_CLI::log( "Done; see vtd_newsbriefs.log" );
+		WP_CLI::log( sprintf( "Done; see %s", $log ) );
+	}
+
+	/**
+	 * Callable for `newspack-content-migrator vtdigger-migrate-liveblogs`.
+	 *
+	 * @param array $pos_args   Positional arguments.
+	 * @param array $assoc_args Associative arguments.
+	 *
+	 * @return void
+	 */
+	public function cmd_liveblogs( array $pos_args, array $assoc_args ) {
+		global $wpdb;
+		$log = 'vtd_liveblog.log';
+
+		// Get Liveblogs category ID.
+		$liveblogs_cat_id = get_cat_ID( self::LIVEBLOGS_CAT_NAME );
+		if ( ! $liveblogs_cat_id ) {
+			$liveblogs_cat_id = wp_insert_category( [ 'cat_name' => self::LIVEBLOGS_CAT_NAME ] );
+		}
+
+		$liveblogs_ids = $wpdb->get_col( "select ID from {$wpdb->posts} where post_type='liveblog';" );
+
+		// Convert to 'post' type.
+		foreach ( $liveblogs_ids as $key_liveblog_id => $liveblog_id ) {
+			WP_CLI::log( sprintf( "(%d)/(%d) %d", $key_liveblog_id + 1, count( $liveblogs_ids ), $liveblog_id ) );
+
+			// Update to type post.
+			$wpdb->query( $wpdb->prepare( "update {$wpdb->posts} set post_type='post' where ID=%d;", $liveblog_id ) );
+
+			// Set meta 'newspack_vtd_cpt' = 'liveblog';
+			update_post_meta( $liveblog_id, self::META_VTD_CPT, 'liveblog' );
+		}
+
+		$this->logger->log( $log, implode( ',', $liveblogs_ids ), false );
+		wp_cache_flush();
+
+		// Assign category 'Liveblogs'.
+		WP_CLI::log( sprintf( "Assigning %s cat ID %d ...", self::LIVEBLOGS_CAT_NAME, $liveblogs_cat_id ) );
+		foreach ( $liveblogs_ids as $key_liveblog_id => $liveblog_id ) {
+			wp_set_post_categories( $liveblog_id, [ $liveblogs_cat_id ] );
+		}
+
+		wp_cache_flush();
+		WP_CLI::log( sprintf( "Done; see %s", $log ) );
 	}
 }
