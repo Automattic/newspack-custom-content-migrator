2a3c63c5a4ad9ab5292e4eff05f41442638ba383
Philip John <phil@philipjohn.me.uk>
2023-02-22 13:40:34 +0000

    Logic: Introduce a Sponsors logic helper

---
+++ src/Logic/Sponsors.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Logic for working with Newspack Sponsors
+ *
+ * @package NewspackCustomContentMigrator
+ */
+
+namespace NewspackCustomContentMigrator\Logic;
+
+use \NewspackCustomContentMigrator\Utils\Logger;
+
+class Sponsors {
+
+	/**
+	 * @var string Sposnors Post Type.
+	 */
+	const SPONSORS_POST_TYPE = 'newspack_spnsrs_cpt';
+
+	/**
+	 * @var string Sponsors Taxonomy.
+	 */
+	const SPONSORS_TAXONOMY = 'newspack_spnsrs_tax';
+
+	/**
+	 * @var Logger.
+	 */
+	private $logger;
+
+	/**
+	 * Constructor.
+	 */
+	private function __construct() {
+		$this->logger = new Logger();
+	}
+
+	/**
+	 * Singleton get_instance().
+	 *
+	 * @return InterfaceCommand|null
+	 */
+	public static function get_instance() {
+		$class = get_called_class();
+		if ( null === self::$instance ) {
+			self::$instance = new $class();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Assign a sponsor to a post
+	 *
+	 * @param int $sponsor ID of the sponsor post
+	 * @param int $post    ID of the post to be sponsored
+	 *
+	 * @return bool True is successful, false on failure.
+	 */
+	public function add_sponsor_to_post( $sponsor, $post ) {
+
+		// Make sure we have a sponsor post.
+		$sponsor_post = get_post( $sponsor );
+		if ( ! is_a( $sponsor_post, 'WP_Post' ) ) {
+			$this->logger->log( 'sponsors', sprintf( 'No sponsor found with ID %d', $sponsor ) );
+			return false;
+		}
+
+		// Check it's definitely a sponsor.
+		if ( $sponsor_post->post_type !== self::SPONSORS_POST_TYPE ) {
+			$this->logger->log( 'sponsors', sprintf( 'Post ID %d is not a sponsor!', $sponsor ) );
+			return false;
+		}
+
+		// Make sure the target post exists, too.
+		$target_post = get_post( $post );
+		if ( ! is_a( $target_post, 'WP_Post' ) ) {
+			$this->logger->log( 'sponsors', sprintf( 'No target post found with ID %d', $sponsor ) );
+			return false;
+		}
+
+		// Get the sponsor term.
+		$sponsor_term = get_term_by( 'name', $sponsor_post->post_title );
+		if ( ! is_a( $sponsor_term, 'WP_Term' ) ) {
+			$this->logger->log( 'sponsors', sprintf( 'No sponsor term found for sponsor %s', $sponsor_post->post_title ) );
+			return false;
+		}
+
+		// Add the Sponsor term to the target post.
+		$add_terms = wp_set_object_terms( $sponsor_post->ID, $sponsor_term->term_id, self::SPONSORS_TAXONOMY, true );
+		if ( is_wp_error( $add_terms ) ) {
+			$this->logger->log( 'sponsors', sprintf( 'Failed to add sponsor term to post %d', $target_post->ID ) );
+			return false;
+		}
+
+		return true;
+
+	}
+}
