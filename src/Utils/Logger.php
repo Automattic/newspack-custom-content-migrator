677a3fdd7f7398e973eda87ffc1d77f3fd1d0f9a
Philip John <phil@philipjohn.me.uk>
2023-02-22 16:27:41 +0000

    Utils: Improve logger location, incl. creating directory

--- src/Utils/Logger.php
+++ src/Utils/Logger.php
@@ -21,8 +21,15 @@ class Logger {
 	 *
 	 * @return string Directory path.
 	 */
-	public function get_le_log_path() {
-		return get_temp_dir() . self::LE_LOG_DIRECTORY;
+	public function get_le_log_path( $filename ) {
+
+		$log_dir = get_temp_dir() . self::LE_LOG_DIRECTORY;
+		if ( ! file_exists( $log_dir ) ) {
+			mkdir( $log_dir );
+		}
+
+		return $log_dir . DIRECTORY_SEPARATOR . $filename . '.log';
+
 	}
 
 	/**
@@ -33,10 +40,15 @@ class Logger {
 	 * @param boolean $to_cli Whether to output the message to the CLI. Default to false.
 	 */
 	public function log( $file, $message, $to_cli = true ) {
-		$message .= "\n";
+
+		// Print the message to the console, if required.
 		if ( $to_cli ) {
 			WP_CLI::line( $message );
 		}
+
+		// Log to the file, ensureing we use a good filename and path.
+		$file     = $this->get_le_log_path( $file );
+		$message .= "\n";
 		file_put_contents( $file, $message, FILE_APPEND );
 	}
 
