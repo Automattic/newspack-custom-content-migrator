4d90a90afc934aeecf0803a5132fcf98dc2d2026
Ivan Uravic <iuravic@gmail.com>
2023-04-10 21:28:19 +0200

    More functionality

--- src/Command/General/ReusableBlocksMigrator.php
+++ src/Command/General/ReusableBlocksMigrator.php
@@ -4,6 +4,8 @@ namespace NewspackCustomContentMigrator\Command\General;
 
 use \NewspackCustomContentMigrator\Command\InterfaceCommand;
 use \NewspackCustomContentMigrator\Command\General\PostsMigrator;
+use \NewspackCustomContentMigrator\Logic\Posts as PostsLogic;
+use \NewspackCustomContentMigrator\Utils\Logger as Logger;
 use \WP_CLI;
 
 class ReusableBlocksMigrator implements InterfaceCommand {
@@ -26,6 +28,20 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 		|xims';
 
 	/**
+	 * Posts logic.
+	 *
+	 * @var PostsLogic $post_logic Posts logic.
+	 */
+	private $post_logic;
+
+	/**
+	 * Logger.
+	 *
+	 * @var Logger $logger Logger.
+	 */
+	private $logger;
+
+	/**
 	 * @var null|InterfaceCommand Instance.
 	 */
 	private static $instance = null;
@@ -34,6 +50,8 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 	 * Constructor.
 	 */
 	private function __construct() {
+		$this->post_logic = new PostsLogic();
+		$this->logger     = new Logger();
 	}
 
 	/**
@@ -44,7 +62,7 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 	public static function get_instance() {
 		$class = get_called_class();
 		if ( null === self::$instance ) {
-			self::$instance = new $class;
+			self::$instance = new $class();
 		}
 
 		return self::$instance;
@@ -54,51 +72,178 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 	 * See InterfaceCommand::register_commands.
 	 */
 	public function register_commands() {
-		WP_CLI::add_command( 'newspack-content-migrator export-reusable-blocks', array( $this, 'cmd_export_reusable_blocks' ), [
-			'shortdesc' => 'Exports Reusable Blocks. Exits with code 0 on success or 1 otherwise.',
-			'synopsis'  => [
-				[
-					'type'        => 'assoc',
-					'name'        => 'output-dir',
-					'description' => 'Output directory full path (no ending slash).',
-					'optional'    => false,
-					'repeating'   => false,
+		WP_CLI::add_command(
+			'newspack-content-migrator export-reusable-blocks',
+			array( $this, 'cmd_export_reusable_blocks' ),
+			[
+				'shortdesc' => 'Exports Reusable Blocks. Exits with code 0 on success or 1 otherwise.',
+				'synopsis'  => [
+					[
+						'type'        => 'assoc',
+						'name'        => 'output-dir',
+						'description' => 'Output directory full path (no ending slash).',
+						'optional'    => false,
+						'repeating'   => false,
+					],
 				],
-			],
-		] );
+			] 
+		);
 
-		WP_CLI::add_command( 'newspack-content-migrator import-reusable-blocks', array( $this, 'cmd_import_reusable_blocks_file' ), [
-			'shortdesc' => 'Imports Reusable Blocks which were exported from the Staging site.',
-			'synopsis'  => [
-				[
-					'type'        => 'assoc',
-					'name'        => 'input-dir',
-					'description' => 'Input directory full path (no ending slash).',
-					'optional'    => false,
-					'repeating'   => false,
+		WP_CLI::add_command(
+			'newspack-content-migrator import-reusable-blocks',
+			array( $this, 'cmd_import_reusable_blocks_file' ),
+			[
+				'shortdesc' => 'Imports Reusable Blocks which were exported from the Staging site.',
+				'synopsis'  => [
+					[
+						'type'        => 'assoc',
+						'name'        => 'input-dir',
+						'description' => 'Input directory full path (no ending slash).',
+						'optional'    => false,
+						'repeating'   => false,
+					],
 				],
-			],
-		] );
+			] 
+		);
 
-		WP_CLI::add_command( 'newspack-content-migrator update-reusable-blocks-id', array( $this, 'cmd_update_reusable_blocks_id' ), [
-			'shortdesc' => 'Updates a Reusable Block ID in content.',
-			'synopsis'  => [
-				[
-					'type'        => 'assoc',
-					'name'        => 'id-old',
-					'description' => 'Old ID.',
-					'optional'    => false,
-					'repeating'   => false,
+		WP_CLI::add_command(
+			'newspack-content-migrator update-reusable-blocks-id',
+			array( $this, 'cmd_update_reusable_blocks_id' ),
+			[
+				'shortdesc' => 'Updates a Reusable Block ID in content.',
+				'synopsis'  => [
+					[
+						'type'        => 'assoc',
+						'name'        => 'id-old',
+						'description' => 'Old ID.',
+						'optional'    => false,
+						'repeating'   => false,
+					],
+					[
+						'type'        => 'assoc',
+						'name'        => 'id-new',
+						'description' => 'New ID.',
+						'optional'    => false,
+						'repeating'   => false,
+					],
 				],
-				[
-					'type'        => 'assoc',
-					'name'        => 'id-new',
-					'description' => 'New ID.',
-					'optional'    => false,
-					'repeating'   => false,
+			] 
+		);
+
+		WP_CLI::add_command(
+			'newspack-content-migrator delete-reusable-blocks-from-content',
+			array( $this, 'cmd_delete_reusable_blocks_from_content' ),
+			[
+				'shortdesc' => 'Goes through all --post-types-csv and searches for reusable blocks with given IDs --reusable-block-ids-csv and removes their usage from post_content.',
+				'synopsis'  => [
+					[
+						'type'        => 'assoc',
+						'name'        => '--reusable-block-ids-csv',
+						'description' => 'CSV of reusable block IDs to delete from post_content.',
+						'optional'    => false,
+						'repeating'   => false,
+					],
+					[
+						'type'        => 'assoc',
+						'name'        => '--post-types-csv',
+						'description' => 'CSV of post types to scan and delete reusable blocks from.',
+						'optional'    => true,
+						'repeating'   => false,
+					],
 				],
-			],
-		] );
+			] 
+		);
+	}
+
+	/**
+	 * Callable for export-reusable-blocks. Exits with code 0 on success or 1 otherwise.
+	 *
+	 * @param array $pos_args   Positional arguments.
+	 * @param array $assoc_args Associative arguments.
+	 */
+	public function cmd_delete_reusable_blocks_from_content( $pos_args, $assoc_args ) {
+		global $wpdb;
+
+		$reusable_block_ids = $assoc_args['reusable-block-ids-csv'];
+		$post_types         = isset( $assoc_args['post-types-csv'] ) ? explode( ',', $assoc_args['post-types-csv'] ) : [ 'post', 'page' ];
+		if ( empty( $reusable_block_ids ) ) {
+			WP_CLI::error( 'No reusable block IDs provided.' );
+		}
+
+		// List of post IDs that were updated.
+		$log = 'post_ids__deleted_reusable_blocks';
+
+		WP_CLI::log( 'Getting all post IDs...' );
+		$post_ids = [];
+		foreach ( $post_types as $post_type ) {
+			$post_ids = array_merge( $post_ids, $this->post_logic->get_all_posts_ids( $post_type ) );
+		}
+
+		// Remove reusable blocks from content.
+		foreach ( $post_ids as $key_post_id => $post_id ) {
+			WP_CLI::log( sprintf( '(%d)/(%d) %d', $key_post_id + 1, count( $post_ids ), $post_id ) );
+
+			// Get post_content.
+			$post_content         = $wpdb->get_var( $wpdb->prepare( "SELECT post_content FROM $wpdb->posts WHERE ID = %d", $post_id ) );
+			$post_content_updated = $post_content;
+
+			// Search and remove reusable blocks from post_content.
+			foreach ( $reusable_block_ids as $reusable_block_id ) {
+				$reusable_block_html = $this->get_reusable_block_html( $reusable_block_id );
+
+				/**
+				 * Instead of just deleting the $reusable_block_html, we should also clean up surrounding line breaks.
+				 * Here are examples of how that could look like in post_content, and we'll search and replace them in this order:
+				 *
+				 * 1. reusable block is in the middle of content, surrounded by line breaks:
+				 *    "
+				 *    a
+				 *
+				 *    <!-- wp:block {"ref":401522} /-->
+				 *
+				 *    a
+				 *    "
+				 *    => we replace (line break + reusable block + line break) with (line break).
+				 *
+				 * 2. reusable block is at the beginning of content, something follows:
+				 *    "
+				 *    <!-- wp:block {"ref":401522} /-->
+				 *
+				 *    a
+				 *    "
+				 *    => we replace (reusable block + line break) with nothing.
+				 *
+				 * 3. reusable block is at the end of content, something precedes:
+				 *    "
+				 *    a
+				 *
+				 *    <!-- wp:block {"ref":401522} /-->
+				 *    "
+				 *    => we replace (line break + reusable block) with nothing.
+				 *
+				 * 4. reusable block is the only thing in content, or line breaks are not properly formatted:
+				 *    "
+				 *    <!-- wp:block {"ref":401522} /-->
+				 *    "
+				 *    => we remove the (reusable block).
+				 */
+				$post_content_updated = str_replace( "\n" . $reusable_block_html . "\n", "\n", $post_content_updated );
+				$post_content_updated = str_replace( "\n" . $reusable_block_html, '', $post_content_updated );
+				$post_content_updated = str_replace( $reusable_block_html . "\n", '', $post_content_updated );
+				$post_content_updated = str_replace( $reusable_block_html, '', $post_content_updated );
+			}
+
+			if ( $post_content_updated != $post_content ) {
+				// Update.
+				$wpdb->update( $wpdb->prefix . 'posts', [ 'post_content' => $post_content_updated ], [ 'ID' => $post_id ] );
+
+				// Log.
+				$this->logger->log( $log, sprintf( 'Updated %d', $post_id ) );
+			}
+		}
+
+		// Let $wpdb->update() sink in.
+		wp_cache_flush();
 	}
 
 	/**
@@ -108,7 +253,7 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 	 * @param $assoc_args
 	 */
 	public function cmd_export_reusable_blocks( $args, $assoc_args ) {
-		$output_dir = isset( $assoc_args[ 'output-dir' ] ) ? $assoc_args[ 'output-dir' ] : null;
+		$output_dir = isset( $assoc_args['output-dir'] ) ? $assoc_args['output-dir'] : null;
 		if ( is_null( $output_dir ) || ! is_dir( $output_dir ) ) {
 			WP_CLI::error( 'Invalid output dir.' );
 		}
@@ -116,7 +261,7 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 		$posts = $this->get_reusable_blocks();
 		if ( empty( $posts ) ) {
 			WP_CLI::warning( 'No Reusable Blocks found.' );
-			exit(1);
+			exit( 1 );
 		}
 
 		WP_CLI::line( sprintf( 'Exporting Reusable Blocks...' ) );
@@ -132,6 +277,17 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 	}
 
 	/**
+	 * Gets HTML of Reusable Block
+	 *
+	 * @param int $post_id Post ID.
+	 *
+	 * @return string HTML of Reusable Block.
+	 */
+	private function get_reusable_block_html( int $post_id ): string {
+		return sprintf( '<!-- wp:block {"ref":%d} /-->', $post_id );
+	}
+
+	/**
 	 * Gets all Reusable Blocks.
 	 *
 	 * @param string[] $post_status     `post_status` argument for \WP_Query::construct().
@@ -140,15 +296,17 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 	 * @return array Array of Posts.
 	 */
 	private function get_reusable_blocks(
-		$post_status    = [ 'publish', 'pending', 'draft', 'future', 'private', 'inherit', 'trash' ],
+		$post_status = [ 'publish', 'pending', 'draft', 'future', 'private', 'inherit', 'trash' ],
 		$posts_per_page = -1
 	) {
 		$posts                 = [];
-		$query_reusable_blocks = new \WP_Query( [
-			'posts_per_page' => $posts_per_page,
-			'post_type'      => 'wp_block',
-			'post_status'    => $post_status,
-		] );
+		$query_reusable_blocks = new \WP_Query(
+			[
+				'posts_per_page' => $posts_per_page,
+				'post_type'      => 'wp_block',
+				'post_status'    => $post_status,
+			] 
+		);
 		if ( ! $query_reusable_blocks->have_posts() ) {
 			return $posts;
 		}
@@ -165,7 +323,7 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 	 * @param $assoc_args
 	 */
 	public function cmd_import_reusable_blocks_file( $args, $assoc_args ) {
-		$input_dir = isset( $assoc_args[ 'input-dir' ] ) ? $assoc_args[ 'input-dir' ] : null;
+		$input_dir = isset( $assoc_args['input-dir'] ) ? $assoc_args['input-dir'] : null;
 		if ( is_null( $input_dir ) || ! is_dir( $input_dir ) ) {
 			WP_CLI::error( 'Invalid input dir.' );
 		}
@@ -173,7 +331,7 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 		$import_file = $input_dir . '/' . self::REUSABLE_BLOCKS_FILE;
 		if ( ! is_file( $import_file ) ) {
 			WP_CLI::warning( sprintf( 'Reusable blocks file not found %s.', $import_file ) );
-			exit(1);
+			exit( 1 );
 		}
 
 		WP_CLI::line( 'Importing Reusable Blocks from ' . $import_file . ' ...' );
@@ -192,8 +350,8 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 	 * @param $assoc_args
 	 */
 	public function cmd_update_reusable_blocks_id( $args, $assoc_args ) {
-		$id_old = $assoc_args[ 'id-old' ] ?? null;
-		$id_new = $assoc_args[ 'id-new' ] ?? null;
+		$id_old = $assoc_args['id-old'] ?? null;
+		$id_new = $assoc_args['id-new'] ?? null;
 		if ( is_null( $id_old ) || ! is_null( $id_new ) ) {
 			WP_CLI::error( 'Params --id-old and --id-new are required.' );
 		}
@@ -202,13 +360,15 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 		$blocks_id_changes[ $id_old ] = $id_new;
 
 		// Get Public Posts and Pages which contain Reusable Blocks.
-		$query_public_posts = new \WP_Query( [
-			'posts_per_page' => -1,
-			'post_type'      => [ 'post', 'page' ],
-			'post_status'    => 'publish',
+		$query_public_posts = new \WP_Query(
+			[
+				'posts_per_page' => -1,
+				'post_type'      => [ 'post', 'page' ],
+				'post_status'    => 'publish',
 			// The search param.doesn't work as expected, so commenting it out for now (it's just a small optimization, anyway).
 			// 's'              => '<!-- wp:block'
-		] );
+			] 
+		);
 		if ( ! $query_public_posts->have_posts() ) {
 			return;
 		}
@@ -259,13 +419,15 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 		}
 
 		// Get Public Posts and Pages which contain Reusable Blocks.
-		$query_public_posts = new \WP_Query( [
-			'posts_per_page' => -1,
-			'post_type'      => [ 'post', 'page' ],
-			'post_status'    => 'publish',
+		$query_public_posts = new \WP_Query(
+			[
+				'posts_per_page' => -1,
+				'post_type'      => [ 'post', 'page' ],
+				'post_status'    => 'publish',
 			// The search param.doesn't work as expected, so commenting it out for now (it's just a small optimization, anyway).
 			// 's'              => '<!-- wp:block'
-		] );
+			] 
+		);
 		if ( ! $query_public_posts->have_posts() ) {
 			return;
 		}
@@ -366,7 +528,7 @@ class ReusableBlocksMigrator implements InterfaceCommand {
 	 */
 	public function match_wp_block( $block_name, $subject ) {
 
-		$pattern = sprintf( self::PATTERN_WP_BLOCK_ELEMENT_SELFCLOSING, $block_name );
+		$pattern               = sprintf( self::PATTERN_WP_BLOCK_ELEMENT_SELFCLOSING, $block_name );
 		$preg_match_all_result = preg_match_all( $pattern, $subject, $matches, PREG_OFFSET_CAPTURE );
 
 		return ( false === $preg_match_all_result || 0 === $preg_match_all_result ) ? null : $matches;
