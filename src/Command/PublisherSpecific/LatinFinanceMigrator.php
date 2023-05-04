8438c7cfe7d98c78ec4ac33a349cfa64eb7f23e8
ronchambers <ronchambers@gmail.com>
2023-05-03 20:42:58 -0700

    Work in progress: duplicate author and tag checks, export to csv, tag parent slugs

--- src/Command/PublisherSpecific/LatinFinanceMigrator.php
+++ src/Command/PublisherSpecific/LatinFinanceMigrator.php
@@ -12,9 +12,9 @@ use \WP_CLI;
  */
 class LatinFinanceMigrator implements InterfaceCommand {
 
-	private $pdo;
-	private $authors;
-	private $tags;
+	private $pdo = null;
+	private $authors = array();
+	private $tags = array();
 
 	/**
 	 * @var null|InterfaceCommand Instance.
@@ -74,7 +74,15 @@ class LatinFinanceMigrator implements InterfaceCommand {
 			WP_CLI::error( 'SQL Server error ' . $e->getCode() . ': ' . $e->getMessage() );
 		}  
 
-		// Get tags (will convert to Categories under a parent category named 'Topics')
+		// Set Authors
+		// todo: how to load existing email addreses for authors?  WXR is hard-coded to @example
+		// just do this with local WP db connection? Needs IP address whitelisted!
+		$this->set_authors();
+		$this->export_to_csv( $this->authors, \WP_CONTENT_DIR  . '/latinfinance-authors.csv');
+		$this->check_author_emails();
+		$this->check_author_slugs();
+
+		// Set tags (convert to Categories under a parent category named 'Topics')
 		// result will look like:
 		//		Topics
 		//			-> Bonds
@@ -85,16 +93,12 @@ class LatinFinanceMigrator implements InterfaceCommand {
 		// The WXR doesn't have <wp:category> nodes inside <channel>.  Need this for slug/heirarchy
 		// todo: handle duplicate child categories names across different parents
 		// todo: do we need to import Descriptions from some categories?
-		// todo: preload content type categories: Daily Briefs, Magazine, Web Articles, Free Content
-		$this->tags = $this->get_array_of_nodes('tag');
-		// print_r($tags); exit();
+		$this->set_tags();
+		$this->set_tags_parent_slugs();
+		$this->export_to_csv( $this->tags, \WP_CONTENT_DIR  . '/latinfinance-tags.csv');
+		$this->check_tags_slugs();
 
-		// Get Authors
-		// todo: how to load existing email addreses for authors?  WXR is hard-coded to @example
-		// just do this with local WP db connection?
-		// todo: do any authors have a bio/description?
-		$this->authors = $this->get_array_of_nodes('author');
-		// print_r($authors); exit();
+		exit();
 
 		// Setup data array WXR for post content
 		$data = [
@@ -126,51 +130,41 @@ class LatinFinanceMigrator implements InterfaceCommand {
 			$xml = simplexml_load_string( $row['xml'] );
 			// print_r($xml);
 			
+			$post_authors = $this->get_authors_from_list( (string) $xml->authors );
+
 			$post = [
-				'author'  => 'Ron Author',
-				'categories' => [
-					'Bonds', 
-					'Corporate & Sovereign Strategy',
-					'Debt',
-				],
+				'author'  => $post_authors,
+				'categories' => $this->get_categories_from_list( (string) $xml->tags ),
 				'content' => (string) $xml->body,
 				// todo: attribute createDate?
 				'date'    => (string) $xml->displayDate,
 				// todo: column expireDate?
 				'excerpt' => (string) $xml->snippet,
 				'meta'    => [
-					'newspack_lf_author' => $this->get_authors_from_list( (string) $xml->authors ),
+					'newspack_lf_author' => $post_authors, 
 					'newspack_lf_checksum' => md5( serialize( $row ) ),
 					'newspack_lf_original_id' => $row['nodeId'],
 					'newspack_lf_original_url' => $this->get_url_from_path( $xml['path'] ),
 					'newspack_lf_original_version' => $row['versionId'],
 				],
-				'title'   => $xml['nodeName'],
+				'title'   => (string) $xml['nodeName'],
 				// todo: test existing slugs across different content types for duplicates
-				'url'    => $xml['urlName'],
+				'url'    => (string) $xml['urlName'],
 			];
 
-			// Convert "content type" to category
+			// Convert "content type" to a category
 			switch( $xml['nodeTypeAlias'] ) {
 				case 'dailyBriefArticle': $post['categories'][] = 'Daily Briefs'; break;
 				case 'magazineArticle': $post['categories'][] = 'Magazine'; break;
 				case 'webArticle': $post['categories'][] = 'Web Articles'; break;
 			}
-			
-			//Free Content
-			// $post['categories'][]
-			
+						
 			// <metaKeywords><![CDATA[bond buyback, liability management, Arcos Dorados, McDonald's, Argentina]]></metaKeywords>
-			// <tags><![CDATA[1136]]></tags>
-			// 
 			// <isFree>0</isFree>
-
-
-
 			// <image><![CDATA[64047]]></image>
-				// body: <img
+			// body: <img
 
-			// print_r($post); 
+			// print_r($post); exit();
 
 			$data['posts'][] = $post;
 
@@ -185,28 +179,82 @@ class LatinFinanceMigrator implements InterfaceCommand {
 
 	}
 
-	private function get_array_of_nodes( $content_type ) {
+	private function check_author_emails() {
 
-		$result = $this->pdo->prepare('
-			select cmsDocument.nodeId, cmsContentXML.xml
-			from cmsDocument
-			join cmsContentXML on cmsContentXML.nodeId = cmsDocument.nodeId
-			join cmsContent on cmsContent.nodeId = cmsDocument.nodeId
-			join cmsContentType on cmsContentType.nodeId = cmsContent.contentType
-				and cmsContentType.alias = :content_type
-			where cmsDocument.published = 1	
-		');
-		$result->execute( array( 
-			':content_type' => $content_type 
-		));
+		$emails = array();
 
-		$arr = array();
-		while ( $row = $result->fetch( PDO::FETCH_ASSOC ) ){   
-			$arr[$row['nodeId']] = $row['xml'];
-		}  
+		foreach( $this->authors as $id => $node ) {
+			
+			// email to test
+			$email = $node['email'];
 
-		return $arr;
+			if( empty($email) ) continue;
 
+			// test if already exists (can't have duplicate email addresses)
+			if( isset( $emails[$email] ) ) {
+				$emails[$email]++;
+				WP_CLI::warning( 'Duplicate email "' . $email .'" for node: ' . print_r( $node , true) );				
+			}
+			else {
+				$emails[$email] = 1;
+			}
+		
+		}
+	}
+
+	private function check_author_slugs() {
+
+		$slugs = array();
+
+		foreach( $this->authors as $id => $node ) {
+			
+			// slug to test
+			$slug = $node['slug'];
+
+			// test if already exists (can't have duplicate slugs)
+			if( isset( $slugs[$slug] ) ) {
+				$slugs[$slug]++;
+				WP_CLI::warning( 'Duplicate author "' . $slug .'" for node: ' . print_r( $node , true) );				
+			}
+			else {
+				$slugs[$slug] = 1;
+			}
+		
+		}
+	}
+
+	private function check_tags_slugs() {
+
+		$slugs = array();
+
+		foreach( $this->tags as $id => $node ) {
+			
+			// slug to test
+			$slug = $node['slug'];
+
+			// test if already exists (can't have duplicate tag ("category") slugs)
+			if( isset( $slugs[$slug] ) ) {
+				$slugs[$slug]++;
+				WP_CLI::warning( 'Duplicate tag (category) "' . $slug .'" for node: ' . print_r( $node , true) );				
+			}
+			else {
+				$slugs[$slug] = 1;
+			}
+		
+		}
+	}
+
+	private function export_to_csv( $data, $path ) {
+		$file = fopen($path, 'w');
+		
+		$header = array_keys(reset($data));
+		fputcsv($file, $header);
+
+		foreach ($data as $row) {
+			fputcsv($file, $row);
+		}
+
+		fclose($file);
 	}
 
 	// null
@@ -215,7 +263,6 @@ class LatinFinanceMigrator implements InterfaceCommand {
 	private function get_authors_from_list( $list ) {
 
 		if( empty( $list ) ) return array();
-
 		$ids = explode(',', $list );
 		
 		$out = array();
@@ -227,7 +274,29 @@ class LatinFinanceMigrator implements InterfaceCommand {
 
 	}
 
+	// null
+	// single id
+	// id,id,id
+	private function get_categories_from_list( $list ) {
+
+		if( empty( $list ) ) return array();
+		$ids = explode(',', $list );
+		
+		$out = array();
+		foreach( $ids as $id ) {
+			var_dump($this->tags[$id]);
+			// nodeName="Funds" urlName="funds" 
+			exit();
+			$out[] = $this->tags[$id];
+		}
+
+		return $out;
+
+	}
+
+
 	// example: "-1,1051,1080,32617,32834,32876,32886" (last element is current element)
+	// used by content and tags
 	private function get_url_from_path( $path ) {
 	
 		// remove the -1 and Home path
@@ -253,4 +322,88 @@ class LatinFinanceMigrator implements InterfaceCommand {
 		return $url;
 
 	}
+
+	private function set_authors() {
+
+		$result = $this->pdo->prepare("
+			select cmsDocument.nodeId, cmsContentXML.xml
+			from cmsDocument
+			join cmsContentXML on cmsContentXML.nodeId = cmsDocument.nodeId
+			join cmsContent on cmsContent.nodeId = cmsDocument.nodeId
+			join cmsContentType on cmsContentType.nodeId = cmsContent.contentType
+				and cmsContentType.alias = 'author'
+			where cmsDocument.published = 1	
+		");
+		$result->execute();
+
+		while ( $row = $result->fetch( PDO::FETCH_ASSOC ) ){   
+			
+			$xml = simplexml_load_string( $row['xml'] );
+			
+			$this->authors[ (string) $row['nodeId'] ] = [
+				'id' => (string) $row['nodeId'],
+				'name' => (string) $xml['nodeName'],
+				'slug' => (string) $xml['urlName'],
+				'email' => (string) $xml->email,
+			];
+
+		}  
+
+	}
+
+	private function set_tags( ) {
+
+		$result = $this->pdo->prepare("
+			select cmsDocument.nodeId, cmsContentXML.xml
+			from cmsDocument
+			join cmsContentXML on cmsContentXML.nodeId = cmsDocument.nodeId
+			join cmsContent on cmsContent.nodeId = cmsDocument.nodeId
+			join cmsContentType on cmsContentType.nodeId = cmsContent.contentType
+				and cmsContentType.alias in('tag','tags')
+			where cmsDocument.published = 1	
+		");
+		$result->execute();
+
+		while ( $row = $result->fetch( PDO::FETCH_ASSOC ) ){   
+
+			$xml = simplexml_load_string( $row['xml'] );
+			
+			$this->tags[ (string) $row['nodeId'] ] = [
+				'id' => (string) $row['nodeId'],
+				'name' => (string) $xml['nodeName'],
+				'slug' => (string) $xml['urlName'],
+				'parent_id' => (string) $xml['parentID'],
+				'url' => $this->get_url_from_path( (string) $xml['path'] ),
+				'description' => (string) $xml->sidebarWidget,
+			];
+
+		}  
+
+	}
+
+	private function set_tags_parent_slugs() {
+
+		foreach( $this->tags as $id => $node ) {
+			
+			$parent_id = $node['parent_id'];
+
+			// special case for top level category where parent is "1051/Home"
+			if( $parent_id == '1051' ) {
+				$this->tags[$id]['parent_slug'] = '';
+				continue;	
+			}
+
+			// if parent id doesn't match a node
+			if ( !isset( $this->tags[$parent_id] ) ) {
+				WP_CLI::error( 'Tag parent_id not found for node: ' . print_r( $this->tags[$id] , true) );				
+			}
+
+			// set node's parent slug from parent node's slug
+			$this->tags[$id]['parent_slug'] = $this->tags[$parent_id]['slug'];
+			
+		}
+
+	}
+
+
 }
