2cf67422e8c22d0c74dc8d53791d18b61e2ae4c4
Zakariae Filali <filali.zakariae@gmail.com>
2023-05-12 14:38:17 +0300

    feat(CCMMigrator): add archive tag to archive posts

--- src/Command/PublisherSpecific/CCMMigrator.php
+++ src/Command/PublisherSpecific/CCMMigrator.php
@@ -69,6 +69,30 @@ class CCMMigrator implements InterfaceCommand {
 	 */
 	public function register_commands() {
 		WP_CLI::add_command(
+			'newspack-content-migrator ccm-set-posts-as-archive',
+			[ $this, 'cmd_set_posts_as_archive' ],
+			[
+				'shortdesc' => 'Set migrated posts as archive.',
+				'synopsis'  => [
+					[
+						'type'        => 'assoc',
+						'name'        => 'batch',
+						'description' => 'Batch to start from.',
+						'optional'    => true,
+						'repeating'   => false,
+					],
+					[
+						'type'        => 'assoc',
+						'name'        => 'posts-per-batch',
+						'description' => 'Posts to import per batch',
+						'optional'    => true,
+						'repeating'   => false,
+					],
+				],
+			]
+		);
+
+		WP_CLI::add_command(
 			'newspack-content-migrator ccm-set-post-brands-and-primary-brand',
 			[ $this, 'cmd_set_post_brands_and_primary_brand' ],
 			[
@@ -106,7 +130,7 @@ class CCMMigrator implements InterfaceCommand {
 
 		$meta_query = [
 			[
-				'key'     => '_newspack_migration_brand_migrated',
+				'key'     => '_newspack_migration_set_as_archive',
 				'compare' => 'NOT EXISTS',
 			],
 			[
@@ -179,6 +203,61 @@ class CCMMigrator implements InterfaceCommand {
 			}
 
 			update_post_meta( $post->ID, '_newspack_migration_gallery_migrated_', true );
+		}
+	}
+
+	/**
+	 * Callable for `newspack-content-migrator ccm-set-posts-as-archive`.
+	 *
+	 * @param array $args CLI args.
+	 * @param array $assoc_args CLI args.
+	 */
+	public function cmd_set_posts_as_archive( $args, $assoc_args ) {
+		$log_file        = 'ccm_posts_as_archive.log';
+		$posts_per_batch = isset( $assoc_args['posts-per-batch'] ) ? intval( $assoc_args['posts-per-batch'] ) : 10000;
+		$batch           = isset( $assoc_args['batch'] ) ? intval( $assoc_args['batch'] ) : 1;
+
+		$meta_query = [
+			[
+				'key'     => '_newspack_migration_set_as_archive',
+				'compare' => 'NOT EXISTS',
+			],
+		];
+
+		$total_query = new \WP_Query(
+			[
+				'posts_per_page' => -1,
+				'post_type'      => 'post',
+				'fields'         => 'ids',
+				'no_found_rows'  => true,
+				'meta_query'     => $meta_query, //phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query
+			]
+		);
+
+		WP_CLI::warning( sprintf( 'Total posts: %d', count( $total_query->posts ) ) );
+
+		$query = new \WP_Query(
+			[
+				'post_type'      => 'post',
+				'paged'          => $batch,
+				'posts_per_page' => $posts_per_batch,
+				'meta_query'     => $meta_query, //phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query
+			]
+		);
+
+		$posts       = $query->get_posts();
+		$total_posts = count( $posts );
+
+		foreach ( $posts as $index => $post ) {
+			WP_CLI::line( sprintf( 'Post %d/%d (%d)', $index + 1, $total_posts, $post->ID ) );
+
+			// Add "Archive" tag.
+			wp_set_post_tags( $post->ID, 'Archive', true );
+
+			// Add "Archive" postmeta.
+			update_post_meta( $post->ID, '_newspack_migration_is_archive', true );
+
+			update_post_meta( $post->ID, '_newspack_migration_set_as_archive', true );
 		}
 	}
 }
