8820fb606fa6f6159fbcb3c92ac38fc1a7d28e6b
Ivan Uravic <iuravic@gmail.com>
2023-05-17 19:07:06 +0200

    Put back commented old code

--- src/Logic/SimpleLocalAvatars.php
+++ src/Logic/SimpleLocalAvatars.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Logic for working with the Simple Local Avatars
- * 
+ *
  * @package NewspackCustomContentMigrator
  */
 
@@ -16,21 +16,21 @@ class SimpleLocalAvatars {
 
 	/**
 	 * Avatar meta key
-	 * 
+	 *
 	 * @var string
 	 */
 	const AVATAR_META_KEY = 'simple_local_avatar';
 
 	/**
 	 * Rating meta key
-	 * 
-	 * @var string 
+	 *
+	 * @var string
 	 */
 	const AVATAR_RATING_META_KEY = 'simple_local_avatar_rating';
 
 	/**
 	 * Instance of Simple_Local_Avatars
-	 * 
+	 *
 	 * @var null|Simple_Local_Avatars
 	 */
 	public $simple_local_avatars;
@@ -42,7 +42,7 @@ class SimpleLocalAvatars {
 		$plugins_path = defined( 'WP_PLUGIN_DIR' ) ? WP_PLUGIN_DIR : ABSPATH . 'wp-content/plugins';
 
 		$simple_local_avatars_plugin_file = $plugins_path . '/simple-local-avatars/simple-local-avatars.php';
-		
+
 		if ( is_file( $simple_local_avatars_plugin_file ) && include_once $simple_local_avatars_plugin_file ) {
 			$this->simple_local_avatars = new Simple_Local_Avatars();
 		}
@@ -50,7 +50,7 @@ class SimpleLocalAvatars {
 
 	/**
 	 * Attach an avatar to a user through Simple Local Avatars
-	 * 
+	 *
 	 * @param int    $user_id The user ID.
 	 * @param int    $attachment_id The attachment ID that has the avatar.
 	 * @param string $rating The avatar rating (G, PG, R, X).
@@ -64,6 +64,10 @@ class SimpleLocalAvatars {
 			update_user_meta( $user_id, self::AVATAR_RATING_META_KEY, $rating );
 		}
 
+		// $avatar = get_user_meta( $user_id, self::AVATAR_META_KEY, true );
+		// if ( ! empty( $avatar ) ) {
+		// 	return false;
+		// }
 		$avatar = get_user_meta( $user_id, self::AVATAR_META_KEY, true );
 		if ( ! empty( $avatar ) ) {
 			return false;
@@ -79,7 +83,7 @@ class SimpleLocalAvatars {
 
 	/**
 	 * Check whether the Simple Local Avatars plugin is active or not
-	 * 
+	 *
 	 * @return boolean True if SLA is active, false otherwise
 	 */
 	public function is_sla_plugin_active() {
@@ -88,9 +92,9 @@ class SimpleLocalAvatars {
 
 	/**
 	 * Check whether a user has an avatar or not
-	 * 
+	 *
 	 * @param int $user_id The user ID.
-	 * 
+	 *
 	 * @return boolean True if the user has an avatar, false otherwise
 	 */
 	public function user_has_avatar( $user_id ) {
