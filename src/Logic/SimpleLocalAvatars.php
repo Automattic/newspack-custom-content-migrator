0b3b817956416fe74f867b1b1e7c30b86f4fb495
Ivan Uravic <iuravic@gmail.com>
2023-05-19 16:47:43 +0200

    Rename SLA method

--- src/Logic/SimpleLocalAvatars.php
+++ src/Logic/SimpleLocalAvatars.php
@@ -57,20 +57,12 @@ class SimpleLocalAvatars {
 	 *
 	 * @return boolean True on success, false otherwise
 	 */
-	public function import_avatar( $user_id, $attachment_id, $rating = '' ) {
-		$this->simple_local_avatars->assign_new_user_avatar( (int) $attachment_id, $user_id );
+	public function assign_avatar( $user_id, $attachment_id, $rating = '' ) {
 
+		// Assign avatar and rating.
+		$this->simple_local_avatars->assign_new_user_avatar( (int) $attachment_id, $user_id );
 		if ( '' !== $rating ) {
 			update_user_meta( $user_id, self::AVATAR_RATING_META_KEY, $rating );
-		}
-
-		// $avatar = get_user_meta( $user_id, self::AVATAR_META_KEY, true );
-		// if ( ! empty( $avatar ) ) {
-		// 	return false;
-		// }
-		$avatar = get_user_meta( $user_id, self::AVATAR_META_KEY, true );
-		if ( ! empty( $avatar ) ) {
-			return false;
 		}
 
 		// Check that the avatar was actually imported.
