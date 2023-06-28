abe10d325fd449c73c41f145ce941ba3748ce210
Ivan Uravic <iuravic@gmail.com>
2023-06-28 16:30:53 +0200

    Update version

--- newspack-custom-content-migrator.php
+++ newspack-custom-content-migrator.php
@@ -5,7 +5,7 @@
  * Plugin URI:  https://newspack.blog/
  * Author:      Automattic
  * Author URI:  https://newspack.blog/
- * Version:     1.3.1
+ * Version:     1.3.2
  *
  * @package  Newspack_Custom_Content_Migrator
  */
