cdaadb9a3b9800aad30faf99574687aadc9e1323
Zakariae Filali <filali.zakariae@gmail.com>
2023-08-20 18:05:48 +0300

    feat(GlobalAtlantaMigrator): migrate regions from XML

--- src/Command/PublisherSpecific/GlobalAtlantaMigrator.php
+++ src/Command/PublisherSpecific/GlobalAtlantaMigrator.php
@@ -59,6 +59,23 @@ class GlobalAtlantaMigrator implements InterfaceCommand {
 				'synopsis'  => [],
 			]
 		);
+
+		WP_CLI::add_command(
+			'newspack-content-migrator global-atlanta-migrate-region-from-xml',
+			[ $this, 'cmd_global_atlanta_migrate_region_from_xml' ],
+			[
+				'shortdesc' => 'Migrate region taxonomy',
+				'synopsis'  => [
+					[
+						'type'        => 'assoc',
+						'name'        => 'xml-file-path',
+						'description' => 'XML file path containing the WP export.',
+						'optional'    => false,
+						'repeating'   => false,
+					],
+				],
+			]
+		);
 	}
 
 	/**
@@ -169,6 +186,100 @@ class GlobalAtlantaMigrator implements InterfaceCommand {
 			[ '%s' ],
 			[ '%s' ]
 		);
+	}
+
+	/**
+	 * Callable for `newspack-content-migrator global-atlanta-migrate-region-from-xml`.
+	 *
+	 * @param $args
+	 * @param $assoc_args
+	 */
+	public function cmd_global_atlanta_migrate_region_from_xml( $args, $assoc_args ) {
+		$xml_file = $assoc_args['xml-file-path'];
+
+		if ( ! file_exists( $xml_file ) ) {
+			WP_CLI::error( 'The provided file does not exist.' );
+		}
+
+		$attachments = []; // [attachment_id => attachment_name].
+		$posts       = []; // [post_id => ['title' => post_title, 'attachment_id' => attachment_id]].
+
+		// Use XMLReader on XML file.
+		$reader = new XMLReader();
+		$reader->open( $xml_file );
+
+		// Read forward to first <item> element.
+		while ( ( $is_item = $reader->read() ) && $reader->name !== 'item' );
+
+		$doc = new DOMDocument();
+		while ( $is_item ) {
+
+			// Here's how to reference namespaces as objects, e.g. properties found in the XML file like xmlns:wp, xmlns:content, xmlns:excerpt.
+			$xml_element = simplexml_import_dom( $reader->expand( $doc ) );
+			$wp_base     = $xml_element->children( 'http://wordpress.org/export/1.2/' );
+
+			$title     = (string) $xml_element->title;
+			$status    = (string) $wp_base->status;
+			$post_type = (string) $wp_base->post_type;
+
+			if ( 'publish' !== $status ) {
+				$is_item = $reader->next( 'item' );
+				continue;
+			}
+
+			for ( $key_category = 0; $key_category + 1 <= $xml_element->category->count(); $key_category++ ) {
+				$category_element = $xml_element->category[ $key_category ];
+				$domain           = (string) $category_element->attributes()['domain'];
+				if ( 'region' === $domain ) {
+					$region             = (string) $category_element;
+					$region_category_id = wp_create_category( $region );
+
+					if ( is_wp_error( $region_category_id ) ) {
+						WP_CLI::warning( sprintf( 'Can\'t create region category: %s: %s', $region, $region_category_id->get_error_message() ) );
+						continue;
+					}
+
+					$live_posts = $this->get_posts_by_title( $title );
+					if ( ! $live_posts ) {
+						WP_CLI::warning( sprintf( 'Post title mismatch: %s (%s)', $title, $post_type ) );
+						continue;
+					}
+
+					foreach ( $live_posts as $live_post ) {
+						if ( $live_post->post_title !== $title ) {
+							WP_CLI::warning( sprintf( 'Post title mismatch: %s (%s)', $title, $post_type ) );
+							continue;
+						}
+
+						// Set category to post.
+						wp_set_post_categories( $live_post->ID, [ $region_category_id ], true );
+
+						// Set category as main category for the post.
+						update_post_meta( $live_post->ID, '_yoast_wpseo_primary_category', $region_category_id );
+
+						WP_CLI::success( sprintf( 'Region category %s is set for the post %d', $region, $live_post->ID ) );
+					}
+				}
+			}
+
+			// Next <item> when done reading this one.
+			$is_item = $reader->next( 'item' );
+		}
+
+		wp_cache_flush();
+	}
+
+	/**
+	 * Get post by title.
+	 *
+	 * @param string $title post title.
+	 * @return object[]|false post object or false if not found.
+	 */
+	private function get_posts_by_title( $title ) {
+		global $wpdb;
+		$sql   = $wpdb->prepare( "SELECT * FROM  $wpdb->posts WHERE post_title = %s AND post_status != 'trash'", $title );
+		$posts = $wpdb->get_results( $sql );
+		return $posts ?? false;
 	}
 
 	/**
