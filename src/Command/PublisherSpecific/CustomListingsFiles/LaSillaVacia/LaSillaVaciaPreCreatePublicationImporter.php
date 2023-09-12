2e4665f523610e78d40689b17ac09a66246592cd
Eddie Carrasco <eddies.code.shop@gmail.com>
2023-09-12 19:46:36 -0400

    fix: necessary changes to fix issues with Publicaciones import

--- src/Command/PublisherSpecific/CustomListingsFiles/LaSillaVacia/LaSillaVaciaPreCreatePublicationImporter.php
+++ src/Command/PublisherSpecific/CustomListingsFiles/LaSillaVacia/LaSillaVaciaPreCreatePublicationImporter.php
@@ -3,6 +3,7 @@
 namespace Newspack_Listings\Importer;
 
 use Newspack_Listings\Contracts\Importer_Mode;
+use NewspackCustomContentMigrator\Command\PublisherSpecific\LaSillaVaciaMigrator;
 
 class LaSillaVaciaPreCreatePublicationImporter extends Abstract_Callable_Pre_Create
 {
@@ -31,7 +32,7 @@ class LaSillaVaciaPreCreatePublicationImporter extends Abstract_Callable_Pre_Cre
             $row['book_publisher'] = '';
             $row['book_buy_or_download'] = 'comprar';
 
-            if ( (int) $row['precio'] > 0 ) {
+            if ( ! $row['free'] ) {
                 $row['book_price'] = "<!-- wp:paragraph -->
                     <p>{$row['precio']} COP</p>
                     <!-- /wp:paragraph -->";
@@ -46,6 +47,20 @@ class LaSillaVaciaPreCreatePublicationImporter extends Abstract_Callable_Pre_Cre
                     <li><strong>ISBN:</strong> {$row['ISBN']}</li>
                     <!-- /wp:list-item -->";
             }
+
+			$row['book_issn'] = '';
+			if ( ! empty( $row['ISSN'] ) ) {
+				$row['book_issn'] = "<!-- wp:list-item -->
+					<li><strong>ISSN:</strong> {$row['ISSN']}</li>
+					<!-- /wp:list-item -->";
+			}
+
+			$row['book_doi'] = '';
+			if ( ! empty( $row['DOI'] ) ) {
+				$row['book_doi'] = "<!-- wp:list-item -->
+					<li><strong>DOI:</strong> {$row['DOI']}</li>
+					<!-- /wp:list-item -->";
+			}
 
             $row['book_published_year'] = '';
             if ( ! empty( $row['bookPublishedAt'] ) ) {
@@ -71,13 +86,56 @@ class LaSillaVaciaPreCreatePublicationImporter extends Abstract_Callable_Pre_Cre
             $row['book_link'] = $row['link'];
 
 
-            $row['images'] = [];
-
             if ( ! is_null( $row['picture'] ) ) {
-                $row['images'][] = [ 'path' => wp_upload_dir()['basedir'] . '/' . $row['picture'] ];
+				$lsv_migrator = LaSillaVaciaMigrator::get_instance();
+				$image = [
+					'FriendlyName' => basename( $row['picture'] ),
+				];
+				global $wpdb;
+				$post_id = $wpdb->get_var(
+					$wpdb->prepare(
+						"SELECT post_id FROM $wpdb->postmeta WHERE meta_key = 'original_post_id' AND meta_value = %d",
+						$row['id']
+					)
+				);
+
+	            if ( $post_id ) {
+//		            $lsv_migrator->handle_featured_image( $image, $row['id'], 0, '/tmp/media_content/bak_Media' );
+		            $featured_image_id = $lsv_migrator->handle_featured_image( $image, $row['id'], 0, '/Users/edc598/Local_Sites/la-silla-vacia-complete-content/app/public/wp-content/Media' );
+
+		            if ( $featured_image_id ) {
+						$row['image_attachment_id'] = $featured_image_id;
+						$row['image_attachment_src'] = wp_get_attachment_image_src( $featured_image_id, 'full' )[0];
+		            }
+	            }
             }
 
-            $row['categories'] = array_map( fn( $category ) => $category['name'], $row['categories'] );
+			$row['book_university'] = '';
+			foreach ( $row['categories'] as &$category ) {
+				$term = get_term_by( 'term_taxonomy_id', $category['term_taxonomy_id'] );
+
+				$category['category'] = $term->term_id;
+
+				if ( str_contains( $category['name'], 'universi' ) ) {
+
+					$link = $category['name'];
+
+					if ( $term ) {
+						$category_link = get_category_link( $term->term_id );
+						$link = '<a href="' . $category_link . '">' . $category['name'] . '</a>';
+					}
+
+					$row['book_university'] = '<!-- wp:paragraph -->' .
+					                          '<p>' . $link . '</p>' .
+											'<!-- /wp:paragraph -->';
+				}
+			}
+
+			$row['tags'] = array_map( function( $tag ) {
+				$tag['tag'] = $tag['name'];
+
+				return $tag;
+			}, $row['tags'] );
         };
     }
 
