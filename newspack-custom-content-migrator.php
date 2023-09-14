358c913c8fc733ad4c43990812223f93b4a94167
Zakariae Filali <filali.zakariae@gmail.com>
2023-09-14 06:29:31 -0400

    Feat/ccm migrator (#341)
    * chore: ignore vscode config
    
    * feat(CCMMigrator): migrate brands
    
    * feat(CCMMigrator): add archive tag to archive posts
    
    * feat(CCMMigrator): migrate co-atuhors, galleries and primary categories
    
    * feat(CCMMigrator): final brands list
    
    * feat(CCMMigrator): save all brands
    
    * fix(CCMMigrator): queries to get posts to migrate
    
    * feat(CCMMigrator): update categories from CSV command

--- newspack-custom-content-migrator.php
+++ newspack-custom-content-migrator.php
@@ -76,6 +76,7 @@ PluginSetup::register_migrators(
 		Command\PublisherSpecific\VTDiggerMigrator::class,
 		Command\PublisherSpecific\SoccerAmericaMigrator::class,
 		Command\PublisherSpecific\MusicallyMigrator::class,
+		Command\PublisherSpecific\CCMMigrator::class,
 		Command\PublisherSpecific\RenoMigrator::class,
 		Command\PublisherSpecific\NewsroomNZMigrator::class,
 		Command\PublisherSpecific\LatinFinanceMigrator::class,
