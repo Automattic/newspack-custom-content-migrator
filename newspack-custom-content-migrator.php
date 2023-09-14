4d47cf70bdaa5ea11a016968bb79d614c8d54cda
Camilla Krag Jensen <naxoc@users.noreply.github.com>
2023-09-14 16:27:27 +0200

    Command to move image credit from posts to attachments
    This is a fix for Mustang News. We need to clean up the class inclusion
    in newspack-custom-content-migrator.php once the command has been run.

--- newspack-custom-content-migrator.php
+++ newspack-custom-content-migrator.php
@@ -84,5 +84,6 @@ PluginSetup::register_migrators(
 		Command\PublisherSpecific\DallasExaminerMigrator::class,
 		Command\PublisherSpecific\BenitoLinkMigrator::class,
 		Command\PublisherSpecific\EfectoCocuyoContentMigrator::class,
+		Command\PublisherSpecific\MustangNewsMigrator::class,
 	)
 );
