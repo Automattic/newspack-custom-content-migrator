4b6cc17f734f1f06f49061802116cea215b35fab
Ivan Uravic <iuravic@gmail.com>
2023-09-20 11:02:53 +0200

    Flourish blocks to Embeds

--- src/Command/PublisherSpecific/BlockClubChicagoMigrator.php
+++ src/Command/PublisherSpecific/BlockClubChicagoMigrator.php
@@ -4,6 +4,8 @@ namespace NewspackCustomContentMigrator\Command\PublisherSpecific;
 
 use \NewspackCustomContentMigrator\Command\InterfaceCommand;
 use \NewspackCustomContentMigrator\Logic\Posts;
+use \NewspackCustomContentMigrator\Logic\GutenbergBlockGenerator;
+use \NewspackContentConverter\ContentPatcher\ElementManipulators\WpBlockManipulator;
 use \WP_CLI;
 
 /**
@@ -24,10 +26,26 @@ class BlockClubChicagoMigrator implements InterfaceCommand {
 	private $posts;
 
 	/**
+	 * WpBlockManipulator instance.
+	 *
+	 * @var WpBlockManipulator WpBlockManipulator instance.
+	 */
+	private $blocks_manipulator;
+
+	/**
+	 * GutenbergBlockGenerator instance.
+	 *
+	 * @var GutenbergBlockGenerator GutenbergBlockGenerator instance.
+	 */
+	private $blocks;
+
+	/**
 	 * Constructor.
 	 */
 	private function __construct() {
-		$this->posts = new Posts();
+		$this->posts              = new Posts();
+		$this->blocks_manipulator = new WpBlockManipulator();
+		$this->blocks             = new GutenbergBlockGenerator();
 	}
 
 	/**
@@ -56,6 +74,61 @@ class BlockClubChicagoMigrator implements InterfaceCommand {
 				'synopsis'  => [],
 			]
 		);
+		WP_CLI::add_command(
+			'newspack-content-migrator blockclubchicago-migrate-flourish-embeds',
+			[ $this, 'cmd_migrate_flourish' ],
+			[
+				'shortdesc' => 'Refactors Flourish to a Gutenberg block.',
+				'synopsis'  => [],
+			]
+		);
+	}
+
+	/**
+	 * @param $pos_args
+	 * @param $assoc_args
+	 *
+	 * @return void
+	 */
+	public function cmd_migrate_flourish( $pos_args, $assoc_args ) {
+		global $wpdb;
+
+		$log = 'flourishes_post_ids.log';
+
+		$post_ids = $this->posts->get_all_posts_ids();
+		foreach ( $post_ids as $key_post_id => $post_id ) {
+			// Get post_content.
+			$post_content = $wpdb->get_var( $wpdb->prepare( "SELECT post_content FROM $wpdb->posts WHERE ID = %d", $post_id ) );
+
+			$matches = $this->blocks_manipulator->match_wp_block_selfclosing( 'wp:lede-common/flourish', $post_content );
+			if ( ! $matches || empty( $matches ) ) {
+				continue;
+			}
+
+			WP_CLI::line( $post_id );
+
+			foreach ( $matches[0] as $match ) {
+				$flourish_html = $match[0];
+				$url = $this->blocks_manipulator->get_attribute( $flourish_html, 'url' );
+
+				$embed_block = $this->blocks->get_core_embed( $url );
+				$embed_html = serialize_block( $embed_block );
+
+				$post_content_updated = $post_content;
+				$post_content_updated = str_replace( $flourish_html, $embed_html, $post_content_updated );
+
+				$wpdb->update(
+					$wpdb->posts,
+					[ 'post_content' => $post_content_updated ],
+					[ 'ID' => $post_id ]
+				);
+
+				file_put_contents( $log, $post_id . "\n", FILE_APPEND );
+			}
+		}
+
+		WP_CLI::line( "Done. See log " . $log );
+		wp_cache_flush();
 	}
 
 	/**
