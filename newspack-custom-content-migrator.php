94a5714fe61ce9c07d87a3661dea98debdb3947e
Ivan Uravic <iuravic@gmail.com>
2023-09-22 17:32:48 +0200

    Bump up version number for CDiff change and release

--- newspack-custom-content-migrator.php
+++ newspack-custom-content-migrator.php
@@ -5,7 +5,7 @@
  * Plugin URI:  https://newspack.blog/
  * Author:      Automattic
  * Author URI:  https://newspack.blog/
- * Version:     1.5.1
+ * Version:     1.5.2
  *
  * @package  Newspack_Custom_Content_Migrator
  */
