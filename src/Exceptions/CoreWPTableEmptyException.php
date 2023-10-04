8eed2e96bf418e0fea93d98ae2093798c5fc9630
Eddie Carrasco <eddies.code.shop@gmail.com>
2023-10-04 13:55:44 -0400

    fix: docblocks to make PHPCS happy.

--- src/Exceptions/CoreWPTableEmptyException.php
+++ src/Exceptions/CoreWPTableEmptyException.php
@@ -1,10 +1,24 @@
 <?php
+/**
+ * CoreWPTableEmptyException class.
+ *
+ * @package NewspackCustomContentMigrator\Exceptions
+ */
 
 namespace NewspackCustomContentMigrator\Exceptions;
 
+/**
+ * Class CoreWPTableEmptyException.
+ *
+ * This class facilitates the handling of instances where core WP tables are empty. It might be necessary
+ * to ignore certain tables even if they are empty. This class will allow us to obtain that list of
+ * empty core tables and allow us to make a decision on a case-by-case basis how to proceed.
+ */
 class CoreWPTableEmptyException extends \Exception {
 
 	/**
+	 * Array of table names that are empty.
+	 *
 	 * @var string[] $tables Names of empty tables.
 	 */
 	protected array $tables = [];
