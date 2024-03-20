729a6304b366078774852364d5529348f68fadc8
Joro Stoyanov <stoyanov.gs@gmail.com>
2024-03-20 09:30:34 +0200

    Add LA Focus migration

---
+++ src/Command/LAFocusMigrator.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Migration tasks for The Emancipator.
+ *
+ * @package NewspackCustomContentMigrator
+ */
+
+namespace NewspackCustomContentMigrator\Command\PublisherSpecific;
+
+use Exception;
+use NewspackCustomContentMigrator\Command\InterfaceCommand;
+use WP_CLI;
+
+/**
+ * Custom migration scripts for The Emancipator.
+ */
+class LAFocusMigrator implements InterfaceCommand {
+
+	/**
+	 * Singleton.
+	 *
+	 * @return LAFocusMigrator
+	 */
+	public static function get_instance(): self {
+		static $instance = null;
+		if ( null === $instance ) {
+			$instance = new self();
+		}
+
+		return $instance;
+	}
+
+    /**
+	 * @throws Exception
+	 */
+	public function register_commands(): void {
+        WP_CLI::add_command(
+			'newspack-content-migrator lafocus-set-featured-image-from-first-image-in-post-content',
+			[ $this, 'cmd_lafocus_set_featured_image_from_first_image_in_post_content' ],
+			[
+				'shortdesc' => 'Searches for the first image in Post Content and uses is a Featured Image in full size.',
+			]
+		);          
+    }
+
+    /** 
+     * Searches for the first image in Post Content and uses is a Featured Image in full size.
+     */
+    public function cmd_lafocus_set_featured_image_from_first_image_in_post_content( array $args, array $assoc_args ): void {
+        WP_CLI::runcommand( 'newspack-content-migrator set-first-image-from-content-as-featured-image' );
+    }
+
+}
