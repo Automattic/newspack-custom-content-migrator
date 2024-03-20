ce6f92a09e5e15a9d491c8afb9a1b8d2e6c62b2c
Joro Stoyanov <stoyanov.gs@gmail.com>
2024-03-20 21:38:11 +0200

    Move migration to publisher specific folder

--- src/Command/PublisherSpecific/LAFocusMigrator.php
+++ src/Command/PublisherSpecific/LAFocusMigrator.php
@@ -30,24 +30,23 @@ class LAFocusMigrator implements InterfaceCommand {
 		return $instance;
 	}
 
-    /**
+	/**
 	 * @throws Exception
 	 */
 	public function register_commands(): void {
-        WP_CLI::add_command(
+		WP_CLI::add_command(
 			'newspack-content-migrator lafocus-set-featured-image-from-first-image-in-post-content',
 			[ $this, 'cmd_lafocus_set_featured_image_from_first_image_in_post_content' ],
 			[
 				'shortdesc' => 'Searches for the first image in Post Content and uses is a Featured Image in full size.',
 			]
 		);          
-    }
+	}
 
-    /** 
-     * Searches for the first image in Post Content and uses it as a Featured Image in full size.
-     */
-    public function cmd_lafocus_set_featured_image_from_first_image_in_post_content( array $args, array $assoc_args ): void {
-        WP_CLI::runcommand( 'newspack-content-migrator set-first-image-from-content-as-featured-image' );
-    }
-
+	/** 
+	 * Searches for the first image in Post Content and uses it as a Featured Image in full size.
+	 */
+	public function cmd_lafocus_set_featured_image_from_first_image_in_post_content( array $args, array $assoc_args ): void {
+		WP_CLI::runcommand( 'newspack-content-migrator set-first-image-from-content-as-featured-image' );
+	}
 }
