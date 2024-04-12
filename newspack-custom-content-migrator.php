bf46ee9f3d6a0654210b38599ca2cecfd166cd5c
Joro Stoyanov <stoyanov.gs@gmail.com>
2024-04-12 21:49:20 +0300

    Add broken images cleanup command

--- newspack-custom-content-migrator.php
+++ newspack-custom-content-migrator.php
@@ -90,5 +90,6 @@ PluginSetup::register_migrators(
 		Command\PublisherSpecific\BigBendSentinelMigrator::class,
 		Command\PublisherSpecific\LAFocusMigrator::class,
 		Command\PublisherSpecific\TheFifthEstateMigrator::class,
+		Command\PublisherSpecific\EastMojoMigrator::class,
 	)
 );
