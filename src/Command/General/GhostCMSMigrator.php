59646f8d04624611c5b6ba966fdd52a82fe54f44
ronchambers <ronchambers@gmail.com>
2024-04-24 13:58:23 -0700

    Ghost CMS Migrator: wip: initial parsing and discovery

--- src/Command/General/GhostCMSMigrator.php
+++ src/Command/General/GhostCMSMigrator.php
@@ -115,71 +115,141 @@ class GhostCMSMigrator implements InterfaceCommand {
 			WP_CLI::error( 'JSON file contained no posts.' );
 		}
 	
-		$cut_off_time = strtotime( "2023-02-17 00:52:24");
-
-		$csv_posts = [];
-		$csv_posts[] = array( 'Created', 'Updated', 'Published', 'Slug', 'Link' );
+		// Insert category/terms.
+		$tags_to_categories = empty( $json->db[0]->data->tags ) ? array() : $this->insert_tags_to_categories( $json->db[0]->data->tags );
+		$users_to_authors   = empty( $json->db[0]->data->users ) ? array() : $this->insert_users_to_authors( $json->db[0]->data->users );
 		
-
+		// Insert posts.
 		foreach( $json->db[0]->data->posts as $json_post ) {
-		
-			if( strtotime( $json_post->created_at ) >= $cut_off_time ) {		
-				$csv_arr = array( $json_post->created_at, $json_post->updated_at, $json_post->published_at, $json_post->slug );
-				$pub_strtotime = ( $json_post->published_at ) ? strtotime( $json_post->published_at ) : 0;
-				if( $pub_strtotime > 0 ) {
-					$csv_arr[] = 'https://www.ourweekly.com/' . date("Y/m/d", $pub_strtotime ) . '/' . $json_post->slug . '/';
-				}
-				$csv_posts[] = $csv_arr;
+
+			// Skip if not post or not published or not visible
+			if( 'post' != $json_post->type || 'published' != $json_post->status || 'public' != $json_post->visibility ) {
 				continue;
 			}
 
-			continue;
+			// Post.
+			$postarr = array(
+				'title' => $json_post->title,
+				'post_content' => $json_post->html,
+				'post_date' => $json_post->published_at,
+				'post_excerpt' => $json_post->custom_excerpt,
+				'post_status' => 'publish',
+			);
 
-			// range of posts near cut off time
-			// if( abs( $cut_off_time - $post_time ) < 60*60*24*7 ) {
+			$post_id = wp_insert_post( $postarr );
+
+			if( ! ( $post_id > 0 ) ) {
+				$this->logger->log( $this->log, 'Could not insert post for json id: ' . $json_post->id, $this->logger::WARNING );
+				continue;
+			}
+
+			// Meta.
+			update_post_meta( $post_id, 'newspack_ghostcms_json_id', $json_post->id );
+			update_post_meta( $post_id, 'newspack_ghostcms_json_uuid', $json_post->uuid );
+			update_post_meta( $post_id, 'newspack_ghostcms_json_slug', $json_post->slug );
+
+			// Add tags as categories.
+			if( ! empty( $json->db[0]->data->posts_tags ) ) {
+				$post_categories = array();
+				foreach( $json->db[0]->data->posts_tags as $json_post_tag ) {
+					if( $json_post_tag->post_id == $json_post->id && isset( $tags_to_categories[ $json_post_tag->tag_id ] ) ) {
+						$post_categories[] = $tags_to_categories[ $json_post_tag->tag_id ];
+					}
+				}
+				if( count( $post_categories ) > 0 ) {
+					wp_set_post_categories( $post_id, $post_categories );
+				}
+			}
+
+			// Authors/CAP GAs.
+			if( ! empty( $json->db[0]->data->posts_authors ) ) {
+				$post_authors = array();
+				foreach( $json->db[0]->data->posts_authors as $json_post_author ) {
+					if( $json_post_author->post_id == $json_post->id && isset( $authors[ $json_post_author->author_id ] ) ) {
+						$post_authors[] = $authors[ $json_post_author->author_id ];
+					}
+				}
+				if( count( $post_authors ) > 0 ) {
+					// add authors to post.
+				}
+			}
+
+			// Fetch "feature_image": "__GHOST_URL__/content/images/wp-content/uploads/2022/10/chaka-khan.jpg",
+			// set _thumbnail_id
 			
+			// Fetch images in content.
 
-			// $sql = $wpdb->prepare("
-			// 	SELECT ID, post_modified
-			// 	FROM wp_posts 
-			// 	where post_type in ( 'page', 'post' ) and post_status = 'publish'
-			// 	and post_date = %s and post_date = %s and post_name = %s
-			// 	", $json_post->created_at, $json_post->published_at, $json_post->slug
-			// );
+			// Set Yoast primary if needed?
 
-			// $results = $wpdb->get_results( $sql );
-
-			// if( 0 == count( $results ) ) {
-			// 	array_unshift( $csv_arr, 'notfound' );
-			// 	$csv_posts[] = $csv_arr;
-			// 	continue;
-			// }
-
-			// if( 1 != count( $results ) ) {
-			// 	WP_CLI::error( 'Duplicate posts in wordpress?' );
-			// }
-
-			// // Single post.
-
-			// // Check if modified
-			// if( strtotime( $results[0]->post_modified ) != strtotime( $json_post->updated_at ) ) {
-			// 	array_unshift( $csv_arr, 'modified' );
-			// 	$csv_posts[] = $csv_arr;
-			// 	continue;
-			// }
+			// Set slug redirects if needed?
 
 		}
 
-		$fp = fopen('file'.time().'.csv', 'w');
-		foreach ($csv_posts as $fields) {
-			fputcsv($fp, $fields);
-		}
-		fclose($fp);
-        
         
         $this->logger->log( $this->log, 'Done.', $this->logger::SUCCESS );
         
 	}
 
+	private function insert_tags_to_categories( $tags ) {
+
+		$out = [];
+
+		foreach( $tags as $tag ) {
+			
+			if( 'public' != $tag->visibility ) continue;
+
+			$term_arr = wp_insert_term( $tag->name, 'category' );
+
+			if( ! is_array( $term_arr ) || empty( $term_arr['term_id'] ) ) {
+				$this->logger->log( $this->log, 'Insert term failed for json tag name: ' . $tag->name, $this->logger::WARNING );
+			}
+			
+			$term = get_term( $term_arr['term_id'] );
+
+			if( $term->slug != $tag->slug ) {
+				// TODO: add category redirect
+			}
+
+			$out[ $tag->id ] = $term_arr['term_id'];
+
+		}
+
+		return $out;
+
+	}
+
+	private function insert_users_to_authors( $users ) {
+
+		$out = [];
+
+		foreach( $users as $user ) {
+			
+			if( 'public' != $user->visibility ) continue;
+
+			// Look up CAP GA or WP User.
+			// "id": "6387a43e354f5f003ddbe55f",
+			// "name": "Our Weekly Staff",
+			// "slug": "adminnewspack",
+
+			// Todo: bio info:
+			// "profile_image": "https://secure.gravatar.com/avatar/bbc27e5236a192d24f7dd18d94b2415c?s=3000&d=mm&r=g",
+			// "cover_image": null,
+			// "bio": null,
+			// "website": "https://ourweekly.com",
+			// "location": null,
+			// "facebook": null,
+			// "twitter": null,
+
+			// insert GA
+
+			// TODO: add slug redirect if different
+
+			// $out[ $user->id ] = new GA id here...
+
+		}
+
+		return $out;
+
+	}
 
 }
