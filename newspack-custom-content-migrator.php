fa8d3b94cf19c8e72b14c765da91887491e72366
Camilla Krag Jensen <naxoc@users.noreply.github.com>
2024-06-14 12:10:06 +0200

    Bump version

--- newspack-custom-content-migrator.php
+++ newspack-custom-content-migrator.php
@@ -5,7 +5,7 @@
  * Plugin URI:  https://newspack.blog/
  * Author:      Automattic
  * Author URI:  https://newspack.blog/
- * Version:     1.5.3
+ * Version:     1.5.4
  *
  * @package  Newspack_Custom_Content_Migrator
  */
