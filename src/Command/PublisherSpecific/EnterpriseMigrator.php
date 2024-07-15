a232dd35b00c2fb8b915da90089ff87d8fb8eb21
Zakariae Filali <filali.zakariae@gmail.com>
2024-07-15 11:13:48 +0100

    feat(EntepriseMigrator): migrate issues

--- src/Command/PublisherSpecific/EnterpriseMigrator.php
+++ src/Command/PublisherSpecific/EnterpriseMigrator.php
@@ -17,6 +17,7 @@ use WP_CLI;
 class EnterpriseMigrator implements InterfaceCommand {
 	const ORIGINAL_ID_META_KEY    = '_newspack_original_id';
 	const STORY_ISSUE_ORIGINAL_ID = '_newspack_story_issue_original_id';
+	const ISSUE_ORIGINAL_ID       = '_newspack_issue_original_id';
 
 	const DEFAULT_AUTHOR = [
 		'display_name' => 'Enterprise Staff',
@@ -96,15 +97,8 @@ class EnterpriseMigrator implements InterfaceCommand {
 			'newspack-content-migrator enterprise-migrate-content',
 			[ $this, 'cmd_enterprise_migrate_content' ],
 			[
-				'shortdesc' => 'Searches all asset JSONs and finds equivalent already imported attachment post_ids. Accuracy of this should be high but it is expected that it is not perfect, HTML galleries for QA and verification of results are produced.',
+				'shortdesc' => 'Command to migrate stories.',
 				'synopsis'  => [
-					[
-						'type'        => 'assoc',
-						'name'        => 'parsing-issue-json-file',
-						'description' => 'The path to the JSON file containing parsing issues (ParsingIssue.json).',
-						'optional'    => false,
-						'repeating'   => false,
-					],
 					[
 						'type'        => 'assoc',
 						'name'        => 'story-json-file',
@@ -114,23 +108,33 @@ class EnterpriseMigrator implements InterfaceCommand {
 					],
 					[
 						'type'        => 'assoc',
-						'name'        => 'story-tag-json-file',
-						'description' => 'The path to the JSON file containing stories tags (StoryTag.json).',
-						'optional'    => false,
-						'repeating'   => false,
-					],
-					[
-						'type'        => 'assoc',
-						'name'        => 'language',
-						'description' => 'The language of the content to be migrated (1: Arabic, 2: English). Default: 2.',
-						'default'     => 2,
-						'optional'    => false,
-						'repeating'   => false,
-					],
-					[
-						'type'        => 'assoc',
 						'name'        => 'brand-id',
 						'description' => 'The brand ID of the content to be migrated.',
+						'optional'    => false,
+						'repeating'   => false,
+					],
+					[
+						'type'        => 'flag',
+						'name'        => 'refresh-content',
+						'description' => 'Refresh content if it already exists.',
+						'default'     => false,
+						'optional'    => true,
+						'repeating'   => false,
+					],
+				],
+			]
+		);
+
+		WP_CLI::add_command(
+			'newspack-content-migrator enterprise-migrate-issues',
+			[ $this, 'cmd_enterprise_migrate_issues' ],
+			[
+				'shortdesc' => 'Migrate issues.',
+				'synopsis'  => [
+					[
+						'type'        => 'assoc',
+						'name'        => 'parsing-issue-json-file',
+						'description' => 'The path to the JSON file containing parsing issues (ParsingIssue.json).',
 						'optional'    => false,
 						'repeating'   => false,
 					],
@@ -156,23 +160,15 @@ class EnterpriseMigrator implements InterfaceCommand {
 	public function cmd_enterprise_migrate_content( $args, $assoc_args ) {
 		$log_file = 'cmd_enterprise_migrate_content.log';
 
-		$parsing_issue_json_file = $assoc_args['parsing-issue-json-file'];
-		$story_json_file         = $assoc_args['story-json-file'];
-		$story_tag_json_file     = $assoc_args['story-tag-json-file'];
-		$language                = $assoc_args['language'];
-		$brand_id                = $assoc_args['brand-id'];
-		$refresh_content         = isset( $assoc_args['refresh-content'] ) ? true : false;
+		$story_json_file = $assoc_args['story-json-file'];
+		$brand_id        = $assoc_args['brand-id'];
+		$refresh_content = isset( $assoc_args['refresh-content'] ) ? true : false;
 
 		$author_id = $this->get_unique_author_id();
 
 		if ( is_wp_error( $author_id ) ) {
 			$this->logger->log( $log_file, ' -- Error creating author: ' . $author_id->get_error_message(), Logger::WARNING );
 			return false;
-		}
-
-		if ( ! in_array( $language, [ 1, 2 ] ) ) {
-			$this->logger->log( $log_file, 'Invalid language. Please provide a valid language (1: Arabic, 2: English).', Logger::ERROR );
-			return;
 		}
 
 		$existing_original_ids = $this->get_existing_original_ids();
@@ -183,10 +179,6 @@ class EnterpriseMigrator implements InterfaceCommand {
 		foreach ( $this->json_iterator->items( $story_json_file ) as $story ) {
 			++$counter;
 			$this->logger->log( $log_file, "Processing story {$counter} of {$total_stories}...", Logger::INFO );
-
-			if ( $language !== $story->LanguageId ) {
-				continue;
-			}
 
 			if ( $story->WebExclude ) {
 				$this->logger->log( $log_file, "Story with ID {$story->StoryId} is excluded from the web. Skipping.", Logger::INFO );
@@ -206,6 +198,47 @@ class EnterpriseMigrator implements InterfaceCommand {
 
 			$wp_story_post_id = $this->migrate_story( $story, $brand_id, $author_id, $log_file );
 			$this->logger->log( $log_file, "Migrated story with ID {$story->StoryId} to post ID {$wp_story_post_id}.", Logger::SUCCESS );
+		}
+	}
+
+	/**
+	 * Callable for `newspack-content-migrator enterprise-migrate-issues`.
+	 *
+	 * @param array $args CLI args.
+	 * @param array $assoc_args CLI args.
+	 */
+	public function cmd_enterprise_migrate_issues( $args, $assoc_args ) {
+		$log_file = 'cmd_enterprise_migrate_issues.log';
+
+		$parsing_issue_file = $assoc_args['parsing-issue-json-file'];
+		$refresh_content    = isset( $assoc_args['refresh-content'] ) ? true : false;
+
+		$author_id = $this->get_unique_author_id();
+
+		if ( is_wp_error( $author_id ) ) {
+			$this->logger->log( $log_file, ' -- Error creating author: ' . $author_id->get_error_message(), Logger::WARNING );
+			return false;
+		}
+
+		$existing_original_ids = $this->get_existing_original_ids( self::STORY_ISSUE_ORIGINAL_ID );
+
+		$total_issues = $this->json_iterator->count_json_array_entries( $parsing_issue_file );
+		$counter      = 0;
+
+		foreach ( $this->json_iterator->items( $parsing_issue_file ) as $issue ) {
+			++$counter;
+			$this->logger->log( $log_file, "Processing issue {$counter} of {$total_issues}...", Logger::INFO );
+
+			// Skip the story if it's already imported and we're not refreshing content.
+			if ( in_array( $issue->IssueId, $existing_original_ids ) && ! $refresh_content ) {
+				$this->logger->log( $log_file, "Issue with ID {$issue->IssueId} already imported. Skipping.", Logger::INFO );
+				continue;
+			}
+
+			$wp_issue_post_id = $this->migrate_issue( $issue, $author_id, $log_file );
+			if ( $wp_issue_post_id ) {
+				$this->logger->log( $log_file, "Migrated issue with ID {$issue->IssueId} to post ID {$wp_issue_post_id}.", Logger::SUCCESS );
+			}
 		}
 	}
 
@@ -289,6 +322,103 @@ class EnterpriseMigrator implements InterfaceCommand {
 	}
 
 	/**
+	 * Migrate issue.
+	 *
+	 * @param object $issue Issue object.
+	 * @param string $author_id Author ID.
+	 * @param string $log_file Log file.
+	 * @return int|WP_Error Issue post ID or WP_Error.
+	 */
+	private function migrate_issue( $issue, $author_id, $log_file ) {
+		// Get all Issues's posts.
+		$issue_posts = get_posts(
+			[
+				'post_type'      => 'post',
+				'posts_per_page' => -1,
+				'meta_key'       => self::STORY_ISSUE_ORIGINAL_ID,
+				'meta_value'     => $issue->IssueId,
+			]
+		);
+
+		if ( empty( $issue_posts ) ) {
+			return false;
+		}
+
+		// Migrate all posts to the issue category.
+		// Create "Issues" the main category.
+		$issue_category_id = wp_create_category( 'Issues' );
+		// Create the issue category.
+		$category_id = wp_create_category( $issue->WebHead, $issue_category_id );
+
+		// Append the category to each post.
+		foreach ( $issue_posts as $issue_post ) {
+			wp_set_post_categories( $issue_post->ID, [ $category_id ], true );
+		}
+
+		// Migrate the issue as a post.
+		$post_content = $this->generate_issue_content( $issue, $issue_posts );
+
+		// Generate post.
+		$post_data = [
+			'post_title'   => $issue->WebHead,
+			'post_content' => $post_content,
+			'post_status'  => $issue->IsPublishedWeb ? 'publish' : 'draft',
+			'post_date'    => $issue->PublishDate->{'$date'},
+			'post_author'  => $author_id,
+			'post_type'    => 'post',
+		];
+
+		// Insert or get the existing post.
+		$wp_post_id = $this->get_post_id_by_meta( self::ORIGINAL_ID_META_KEY, $issue->IssueId );
+
+		if ( ! $wp_post_id ) {
+			$wp_post_id = wp_insert_post( $post_data );
+		} else {
+			$post_data['ID'] = $wp_post_id;
+			wp_update_post( $post_data );
+		}
+
+		// Audio File.
+		if ( ! empty( $issue->IntroductionVoiceUrl ) ) {
+			$audio_file_post_id = $this->attachments->import_external_file( $issue->IntroductionVoiceUrl, null, null, null, null, $wp_post_id );
+
+			if ( is_wp_error( $audio_file_post_id ) ) {
+				$this->logger->log( $log_file, sprintf( 'Could not upload file %s: %s', $issue->IntroductionVoiceUrl, $audio_file_post_id->get_error_message() ), Logger::WARNING );
+			} else {
+				wp_update_post(
+					[
+						'ID'           => $wp_post_id,
+						'post_content' => serialize_block( $this->gutenberg_block_generator->get_audio( $audio_file_post_id ) ) . $post_content,
+					]
+				);
+			}
+		}
+
+		// Post category.
+		$cateogry_id = wp_create_category( 'Issues Posts' );
+		wp_set_post_categories( $wp_post_id, $cateogry_id );
+
+		// Post subtitle.
+		update_post_meta( $wp_post_id, 'newspack_post_subtitle', $issue->IssueTeaser );
+
+		// Featured image.
+		if ( ! empty( $issue->EditionWebImage ) ) {
+			$file_post_id = $this->attachments->import_external_file( $issue->EditionWebImage, null, null, null, null, $wp_post_id );
+
+			if ( is_wp_error( $file_post_id ) ) {
+				$this->logger->log( $log_file, sprintf( 'Could not upload file %s: %s', $issue->EditionWebImage, $file_post_id->get_error_message() ), Logger::WARNING );
+			} else {
+				set_post_thumbnail( $wp_post_id, $file_post_id );
+			}
+		}
+
+		// A few meta fields.
+		update_post_meta( $wp_post_id, self::ISSUE_ORIGINAL_ID, $issue->IssueId );
+
+		return $wp_post_id;
+	}
+
+	/**
 	 * Clean content.
 	 *
 	 * @param string $content Content to clean.
@@ -311,6 +441,53 @@ class EnterpriseMigrator implements InterfaceCommand {
 		}
 
 		return $this->crawler->html();
+	}
+
+	/**
+	 * Generate issue content.
+	 *
+	 * @param object $issue Issue object.
+	 * @param array  $issue_posts Issue posts.
+	 *
+	 * @return string Issue content.
+	 */
+	private function generate_issue_content( $issue, $issue_posts ) {
+		$content_blocks = [];
+
+		// TLDR.
+		$content_blocks[] = $this->gutenberg_block_generator->get_heading( 'TL;DR' );
+
+		// TLDR list.
+		$content_blocks[] = $this->gutenberg_block_generator->get_list(
+			array_map(
+				function ( $issue ) {
+					$text = $issue->TLDRLineText;
+					if ( ! empty( $issue->TLDRLineCategory ) ) {
+						$text .= ' | ' . $issue->TLDRLineCategory;
+					}
+					return $text;
+				},
+				$issue->TLDR
+			)
+		);
+
+		// Homepage Blog Posts block.
+		$content_blocks[] = $this->gutenberg_block_generator->get_homepage_articles_for_specific_posts(
+			array_map(
+				function ( $issue_post ) {
+						return $issue_post->ID;
+				},
+				$issue_posts
+			),
+			[
+				'excerptLength' => 100,
+				'showReadMore'  => false,
+				'showDate'      => false,
+				'showAuthor'    => false,
+			]
+		);
+
+		return serialize_blocks( $content_blocks );
 	}
 
 	/**
@@ -342,12 +519,14 @@ class EnterpriseMigrator implements InterfaceCommand {
 		/**
 		 * Get existing original IDs.
 		 *
+		 * @param string $key Meta key, default is self::ORIGINAL_ID_META_KEY.
+		 *
 		 * @return array Existing original IDs.
 		 */
-	private function get_existing_original_ids() {
+	private function get_existing_original_ids( $key = self::ORIGINAL_ID_META_KEY ) {
 		global $wpdb;
 
-		$existing_original_ids = $wpdb->get_col( $wpdb->prepare( "SELECT meta_value FROM $wpdb->postmeta WHERE meta_key = %s", self::ORIGINAL_ID_META_KEY ) );
+		$existing_original_ids = $wpdb->get_col( $wpdb->prepare( "SELECT meta_value FROM $wpdb->postmeta WHERE meta_key = %s", $key ) );
 
 		return $existing_original_ids;
 	}
